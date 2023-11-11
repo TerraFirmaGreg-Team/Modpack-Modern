6fd2cdbe11a01ba329656ed7dae4359d5f9e380e
SpeeeDCraft <52341158+SpeeeDCraft@users.noreply.github.com>
2023-11-11 23:34:09 +0700

    he2

--- kubejs/client_scripts/tfc/constants.js
+++ kubejs/client_scripts/tfc/constants.js
@@ -304,7 +304,27 @@ const tfcItemsToHide = [
     'tfc:ore/poor_cassiterite', 
     'tfc:ore/rich_native_silver', 
     'tfc:ore/normal_native_silver', 
-    'tfc:ore/poor_native_silver'
+    'tfc:ore/poor_native_silver',
+
+    // Gems
+    'tfc:gem/amethyst', 
+    'tfc:gem/diamond', 
+    'tfc:gem/lapis_lazuli', 
+    'tfc:gem/emerald', 
+    'tfc:gem/pyrite', 
+    'tfc:gem/opal', 
+    'tfc:gem/ruby', 
+    'tfc:gem/sapphire', 
+    'tfc:gem/topaz', 
+    'tfc:ore/amethyst', 
+    'tfc:ore/diamond', 
+    'tfc:ore/lapis_lazuli', 
+    'tfc:ore/emerald', 
+    'tfc:ore/pyrite', 
+    'tfc:ore/opal', 
+    'tfc:ore/ruby', 
+    'tfc:ore/sapphire', 
+    'tfc:ore/topaz'
 ]
 
 const tfcSimpleMaterials = [
