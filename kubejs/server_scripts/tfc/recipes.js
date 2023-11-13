1387978d9cf18cc97ee9fb50a5d579160e0f4787
SpeeeDCraft <52341158+SpeeeDCraft@users.noreply.github.com>
2023-11-13 22:08:16 +0700

    600+ файлов в 200 json строк (ну и кто тут умный)

--- kubejs/server_scripts/tfc/recipes.js
+++ kubejs/server_scripts/tfc/recipes.js
@@ -1,10 +1,12 @@
 // priority: 0
 
 const registerTFCRecipes = (event) => {
-    event.remove({ id: /tfc:anvil.*/ })
-    event.remove({ id: /tfc:welding.*/ })
-    event.remove({ id: /tfc:casting.*/ })
-    event.remove({ id: /tfc:heating.*/ })
+    //event.remove({ id: /tfc:anvil.*/ })
+    //event.remove({ id: /tfc:welding.*/ })
+    //event.remove({ id: /tfc:casting.*/ })
+    event.remove({ id: /tfc:heating\/metal.*/ })
+    event.remove({ id: /tfc:heating\/ore.*/ })
+
 
     event.remove({ id: 'tfc:crafting/vanilla/lapis_block' })
 
