01e4ed51687e3cdde24c0fd37400c5a380e10309
SpeeeDCraft <52341158+SpeeeDCraft@users.noreply.github.com>
2023-11-13 11:06:36 +0700

    remove some jsons again

--- kubejs/server_scripts/tfc/data.js
+++ kubejs/server_scripts/tfc/data.js
@@ -31,9 +31,11 @@ const registerTFCData = (event) => {
     }
 
     for (const [metal, metalSpecs] of Object.entries(metalToSpecs)) {
+        
+        let metalName = (metalSpecs.customName != null) ? metalSpecs.customName : metal
+
         metalSpecs.props.forEach(propName => {
             let json
-            let metalName = (metalSpecs.customName != null) ? metalSpecs.customName : metal
             let property = itemTypeToHeat[propName]
 
             if (property.useTag) json = tagJson(`forge:${property.hasGTVariant}/${metalName}`, property.heat_capacity, metalSpecs.forging_temperature, metalSpecs.welding_temperature) 
@@ -51,6 +53,28 @@ const registerTFCData = (event) => {
             if (property.heat_capacity == 0.0) event.addJson(path, emptyJson)
             else event.addJson(path, json)
         });
+
+        if (metalSpecs.hasOre) {
+            let path = `tfc:tfc/item_heats/ore/${metalName}`
+            let json = {
+                ingredient: [
+                    {
+                      "tag": `forge:poor_raw_materials/${metalName}`
+                    },
+                    {
+                      "tag": `forge:raw_materials/${metalName}`
+                    },
+                    {
+                      "tag": `forge:rich_raw_materials/${metalName}`
+                    }
+                  ],
+                heat_capacity: 1.143,
+                forging_temperature: metalSpecs.forging_temperature,
+                welding_temperature: metalSpecs.welding_temperature
+            }
+
+            event.addJson(path, json)
+        }
     }
 }
 
