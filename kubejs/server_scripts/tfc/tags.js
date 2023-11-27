f51bb2e970b906e2e1e9f63528085e34d17880f2
Dmitry <ondyshev.dmitry@gmail.com>
2023-11-27 08:48:22 +0700

    Исправлен очередной дюп металла

--- kubejs/server_scripts/tfc/tags.js
+++ kubejs/server_scripts/tfc/tags.js
@@ -5,6 +5,13 @@ const registerTFCItemTags = (event) => {
         event.removeAllTagsFrom(item)
     })
 
+    event.remove('forge:dusts/iron', 'tfc:powder/limonite')
+    event.remove('forge:dusts/iron', 'tfc:powder/magnetite')
+    event.remove('forge:dusts/iron', 'tfc:powder/hematite')
+    event.remove('forge:dusts/copper', 'tfc:powder/malachite')
+    event.remove('forge:dusts/copper', 'tfc:powder/copper')
+    event.remove('forge:dusts/copper', 'tfc:powder/tetrahedrite')
+
     event.add('forge:ingots/double/bismuth', 'tfc:metal/double_ingot/bismuth')
     event.add('forge:ingots/double/bismuth_bronze', 'tfc:metal/double_ingot/bismuth_bronze')
     event.add('forge:ingots/double/black_bronze', 'tfc:metal/double_ingot/black_bronze')
