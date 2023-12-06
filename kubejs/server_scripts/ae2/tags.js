d63feca6acfc042306506cef5429eac8fcc6ca71
FriendlyCube <friendlycube20@gmail.com>
2023-12-06 15:31:55 +0200

    чуточку г*внокода
    главное признать свои ошибки

---
+++ kubejs/server_scripts/ae2/tags.js
@@ -0,0 +1,10 @@
+// priority: 0
+
+const registerAE2Tags = (event) => {
+    //certus_quartz
+    event.add('forge:rods/certus_quartz', 'kubejs:rods/certus_quartz');
+    event.add('forge:rods', 'kubejs:rods/certus_quartz');
+    //nether_quartz
+    event.add('forge:rods/nether_quartz', 'kubejs:rods/nether_quartz');
+    event.add('forge:rods', 'kubejs:rods/nether_quartz');
+}
