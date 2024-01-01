eed63dd30fb99409f2c4c1a4911572344eec6326
Dmitry <ondyshev.dmitry@gmail.com>
2024-01-01 13:37:27 +0700

    some fixes

--- kubejs/server_scripts/firmaciv/recipes.js
+++ kubejs/server_scripts/firmaciv/recipes.js
@@ -4,4 +4,7 @@ const registerFirmaCivRecipes = (event) => {
     
     event.remove({ id: '/firmaciv:quern/' })
         
+    event.recipes.tfc.heating('firmaciv:oarlock', 1535)
+        .resultFluid(Fluid.of('tfc:metal/cast_iron', 288))
+        .id(`tfg:firmaciv/heating/oarlock`)
 }
