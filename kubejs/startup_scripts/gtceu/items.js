1ba515445304bf0bc767c26ba5993edee429059e
Xikaro <0regon.end@gmail.com>
2024-01-04 16:34:54 +0500

    mod id

--- kubejs/startup_scripts/gtceu/items.js
+++ kubejs/startup_scripts/gtceu/items.js
@@ -1,6 +1,6 @@
 // priority: 0
 
 const registerGTCEuItems = (event) => {
-    event.create('terrafirmagreg:unfinished_vacuum_tube', 'create:sequenced_assembly')
-    event.create('terrafirmagreg:unfinished_basic_electronic_circuit', 'create:sequenced_assembly')
+    event.create('tfg:unfinished_vacuum_tube', 'create:sequenced_assembly')
+    event.create('tfg:unfinished_basic_electronic_circuit', 'create:sequenced_assembly')
 }
