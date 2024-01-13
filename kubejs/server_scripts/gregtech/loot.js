34c81b6ecdc8306fc191ba241dff13679f5306ea
Dmitry <ondyshev.dmitry@gmail.com>
2024-01-13 10:59:45 +0700

    stylize

--- kubejs/server_scripts/gregtech/loot.js
+++ kubejs/server_scripts/gregtech/loot.js
@@ -5,13 +5,22 @@ const registerGTCEULoots = (event) => {
     global.TFC_STONE_TYPES.forEach(stoneType => {
         GTRegistries.MATERIALS.forEach(material => {
             if (material.hasProperty(PropertyKey.ORE)) {
-                event.addBlockLootModifier(`gtceu:${stoneType}_${material}_ore`)
-                    .removeLoot(Ingredient.all)
-                    .addWeightedLoot([
-                        ChemicalHelper.get(TFGTagPrefix.richRawOre, material, 1).withChance(0.2),
-                        ChemicalHelper.get(TagPrefix.rawOre, material, 1).withChance(0.6),
-                        ChemicalHelper.get(TFGTagPrefix.poorRawOre, material, 1).withChance(0.2)
-                    ]);
+                
+                let richRawOre = ChemicalHelper.get(TFGTagPrefix.richRawOre, material, 1)
+                let normalRawOre = ChemicalHelper.get(TagPrefix.rawOre, material, 1)
+                let poorRawOre = ChemicalHelper.get(TFGTagPrefix.poorRawOre, material, 1).withChance(0.2)
+
+                if (!richRawOre.isEmpty() && !normalRawOre.isEmpty() && !poorRawOre.isEmpty()) {
+                    
+                    event.addBlockLootModifier(`gtceu:${stoneType}_${material}_ore`)
+                        .removeLoot(Ingredient.all)
+                        .addWeightedLoot([
+                            richRawOre.withChance(0.2),
+                            normalRawOre.withChance(0.6),
+                            poorRawOre.withChance(0.2)
+                        ]);
+                }
+
             }
         })
     })
