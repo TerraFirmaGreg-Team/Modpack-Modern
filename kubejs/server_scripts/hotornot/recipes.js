3cfd06243fd36f6adadb27e06923f4e54de9812b
Dmitry <ondyshev.dmitry@gmail.com>
2024-04-25 20:16:37 +0700

    fixes

--- kubejs/server_scripts/hotornot/recipes.js
+++ kubejs/server_scripts/hotornot/recipes.js
@@ -2,6 +2,9 @@
 
 const registerHotOrNotRecipes = (event) => {
     
+    event.remove({ id: 'tfchotornot/heating/tong_part/cast_iron' })
     event.remove({ id: 'tfchotornot/anvil/tong_part/cast_iron' })
+
+    event.remove({ id: 'tfchotornot/heating/tongs/cast_iron' })
     event.remove({ id: 'tfchotornot:crafting/tongs/cast_iron' })
 }
