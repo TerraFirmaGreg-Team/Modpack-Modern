c532e1182b751f124cca2555bd8eb0c4294e9974
Dmitry <ondyshev.dmitry@yandex.ru>
2024-08-31 15:13:28 +0700

    большой рефакторинг продолжается

--- kubejs/server_scripts/ae2/tags.js
+++ kubejs/server_scripts/ae2/tags.js
@@ -4,15 +4,15 @@ const registerAE2ItemTags = (event) => {
     
     event.add('c:hidden_from_recipe_viewers', "ae2:facade")
 
-    global.AE2_DISABLED_ITEMS.forEach(item => {
-        event.removeAllTagsFrom(item)
-        event.add('c:hidden_from_recipe_viewers', item)
-    })
+    // global.AE2_DISABLED_ITEMS.forEach(item => {
+    //     event.removeAllTagsFrom(item)
+    //     event.add('c:hidden_from_recipe_viewers', item)
+    // })
 }
 
 const registerAE2BlockTags = (event) => {
     
-    global.AE2_DISABLED_ITEMS.forEach(item => {
-        event.removeAllTagsFrom(item)
-    })
+    // global.AE2_DISABLED_ITEMS.forEach(item => {
+    //     event.removeAllTagsFrom(item)
+    // })
 }
