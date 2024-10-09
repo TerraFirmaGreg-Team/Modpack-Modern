1c557771b8161509c97b782bcd292cfa8f11dcb0
Dmitry <ondyshev.dmitry@yandex.ru>
2024-10-09 10:54:37 +0700

    restore all!

--- kubejs/server_scripts/chisel_and_bits/tags.js
+++ kubejs/server_scripts/chisel_and_bits/tags.js
@@ -2,23 +2,21 @@
 
 const registerChiselAndBitsItemTags = (event) => {
     
-    if (Platform.isLoaded('chiselsandbits')) {
-        event.add('c:hidden_from_recipe_viewers', "chiselandbits:block_bit")
+    if (!Platform.isLoaded('chiselsandbits')) return
+
+    event.add('c:hidden_from_recipe_viewers', "chiselandbits:block_bit")
 
         global.CHISEL_AND_BITS_DISABLED_ITEMS.forEach(item => {
             event.removeAllTagsFrom(item)
             event.add('c:hidden_from_recipe_viewers', item)
         })
-    }
 }
 
 const registerChiselAndBitsBlockTags = (event) => {
     
-    if (Platform.isLoaded('chiselsandbits')) {
-        global.CHISEL_AND_BITS_DISABLED_ITEMS.forEach(item => {
-            event.removeAllTagsFrom(item)
-        })
-    }
+    if (!Platform.isLoaded('chiselsandbits')) return
 
-    
+    global.CHISEL_AND_BITS_DISABLED_ITEMS.forEach(item => {
+        event.removeAllTagsFrom(item)
+    })
 }
