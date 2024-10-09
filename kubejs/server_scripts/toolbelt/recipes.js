c2cfd9d9ff40b59781c6e4f4801153de4742cfe4
Dmitry <ondyshev.dmitry@yandex.ru>
2024-10-09 10:30:09 +0700

    restore morered + tfcgrrr + tfcaea + toolbelts

--- kubejs/server_scripts/toolbelt/recipes.js
+++ kubejs/server_scripts/toolbelt/recipes.js
@@ -14,7 +14,7 @@ const registerToolBeltRecipes = (event) => {
         A: '#forge:string',
         B: '#forge:leather',
         C: '#forge:plates/wrought_iron'
-    }).id('tfg:toolbelt/shaped/pouch')
+    }).id('tfg:shaped/pouch')
 
     // ToolBelt с размером 2
     event.shaped(Item.of('toolbelt:belt', {Size:2}), [
@@ -25,13 +25,13 @@ const registerToolBeltRecipes = (event) => {
         A: '#forge:string',
         B: '#forge:leather',
         C: '#forge:plates/wrought_iron'
-    }).id('tfg:toolbelt/shaped/belt_' + 2)
+    }).id('tfg:shaped/belt_2')
 
     // ToolBelt с размером 3 - 9
     for (let i = 3; i < 10; i++) {
         event.shapeless(Item.of('toolbelt:belt', {Size:i}), [
             Ingredient.of(Item.of('toolbelt:belt', {Size:i-1}).strongNBT()).or(Ingredient.of(Item.of('toolbelt:belt', {Size:i-1,Items:[]}).strongNBT())),
             'toolbelt:pouch'
-        ]).id('tfg:toolbelt/shaped/belt_' + i)
+        ]).id(`tfg:shaped/belt_${i}`)
     }
 }
