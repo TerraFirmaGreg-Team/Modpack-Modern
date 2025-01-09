543162157434eb31beed2592f936b5f79f3f150a
Pyritie <pyritie@gmail.com>
2025-01-09 23:18:10 +0000

    multiblock definitions
    Signed-off-by: Pyritie <pyritie@gmail.com>

--- kubejs/startup_scripts/gtceu/recipe_types.js
+++ kubejs/startup_scripts/gtceu/recipe_types.js
@@ -15,5 +15,22 @@ const registerGTCEuRecipeTypes = (event) => {
         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.ARC)
         .setMaxTooltips(6)
+		
+		
+	event.create('nether_dome')
+		.category('nether_dome')
+		.setEUIO('in')
+		.setMaxIOSize(1, 1, 1, 1)
+		.setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_GAS_COLLECTOR, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.FIRE)
+		
+	event.create('end_dome')
+		.category('end_dome')
+		.setEUIO('in')
+		.setMaxIOSize(1, 1, 1, 1)
+		.setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_GAS_COLLECTOR, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.FIRE)
 }
 
