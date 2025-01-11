e73f36391133ad1714fcc38200aeb94da3d58741
Pyritie <pyritie@gmail.com>
2025-01-11 12:40:57 +0000

    Update recipe_types.js
    Signed-off-by: Pyritie <pyritie@gmail.com>

--- kubejs/startup_scripts/gtceu/recipe_types.js
+++ kubejs/startup_scripts/gtceu/recipe_types.js
@@ -7,13 +7,5 @@ const registerGTCEuRecipeTypes = (event) => {
         .setMaxIOSize(3, 4, 1, 0)
         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.BATH)
-
-    event.create('alternator')
-        .category('alternator')
-        .setEUIO('out')
-        .setMaxIOSize(1, 0, 0, 0)
-        .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
-        .setSound(GTSoundEntries.ARC)
-        .setMaxTooltips(6)
 }
 
