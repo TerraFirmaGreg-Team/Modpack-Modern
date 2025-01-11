9459b2088872cf0e8e2a137131e24f48a48a134a
Pyritie <pyritie@gmail.com>
2025-01-11 12:41:11 +0000

    Update machines.js
    Signed-off-by: Pyritie <pyritie@gmail.com>

--- kubejs/startup_scripts/gtceu/machines.js
+++ kubejs/startup_scripts/gtceu/machines.js
@@ -23,25 +23,5 @@ const registerGTCEuMachines = (event) => {
             .build()
         )
         .workableCasingRenderer('gtceu:block/casings/solid/machine_casing_solid_steel', 'gtceu:block/multiblock/implosion_compressor', false)
-
-    event.create('alternator', 'multiblock')
-        .rotationState(RotationState.NON_Y_AXIS)
-        .recipeType('alternator')
-        .pattern(definition => FactoryBlockPattern.start()
-            .aisle("CWC", "CWC", "#W#")
-            .aisle("CWC", "K#E", "CWC")
-            .aisle("CWI", "CWA", "#W#")
-            .where('A', Predicates.controller(Predicates.blocks(definition.get())))
-            .where('W', Predicates.blocks('gtceu:cupronickel_coil_block'))
-            .where("C", Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get()))
-            .where('#', Predicates.any())
-            .where('K', Predicates.abilities(PartAbility.INPUT_KINETIC).setExactLimit(1))
-            .where('E', Predicates.abilities(PartAbility.OUTPUT_ENERGY).setExactLimit(1))
-            .where('I', Predicates.abilities(PartAbility.IMPORT_ITEMS).setExactLimit(1))
-            .build()
-        )
-        .workableCasingRenderer(
-            "gtceu:block/casings/solid/machine_casing_solid_steel",
-            "gtceu:block/multiblock/implosion_compressor", false
-        )
+    
 }
