62a80d44af053da7f1ce6df878c44c7241c47938
SverhRazum-Nah <leon.trol@mail.ru>
2025-01-13 19:40:26 +0300

    #654 duplicate
    Signed-off-by: SverhRazum-Nah <leon.trol@mail.ru>

--- kubejs/server_scripts/minecraft/recipes.js
+++ kubejs/server_scripts/minecraft/recipes.js
@@ -3270,4 +3270,37 @@ const registerMinecraftRecipes = (event) => {
 		.EUt(32)
 
     //#endregion
+
+    //#region Кожа из кожаных предметов в дробилке
+    e.recipes.gtceu.macerator('tfg:leather_from_boots')
+    	.itemInputs('minecraft:leather_boots')
+    	.itemOutputs('minecraft:leather')
+    	.EUt(7).duration(80)
+
+    e.recipes.gtceu.macerator('tfg:leather_from_saddle')
+    	.itemInputs('minecraft:saddle')
+    	.itemOutputs('minecraft:leather')
+    	.EUt(7).duration(80)
+
+    e.recipes.gtceu.macerator('tfg:leather_from_horse_armor')
+    	.itemInputs('minecraft:leather_horse_armor')
+    	.itemOutputs('minecraft:leather')
+    	.EUt(7).duration(80)
+
+    e.recipes.gtceu.macerator('tfg:leather_from_helmet')
+    	.itemInputs('minecraft:leather_helmet')
+    	.itemOutputs('minecraft:leather')
+    	.EUt(7).duration(80)
+
+    e.recipes.gtceu.macerator('tfg:leather_from_leggings')
+    	.itemInputs('minecraft:leather_leggings')
+    	.itemOutputs('minecraft:leather')
+    	.EUt(7).duration(80)
+
+    e.recipes.gtceu.macerator('tfg:leather_from_chestplate')
+    	.itemInputs('minecraft:leather_chestplate')
+    	.itemOutputs('minecraft:leather')
+    	.EUt(7).duration(80)
+    //#endregion
+	
 }
