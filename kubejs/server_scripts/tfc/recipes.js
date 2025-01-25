564a1a1089ae23c0d69a13f8d5c08ef4d11495a4
SverhRazum-Nah <leon.trol@mail.ru>
2025-01-26 02:53:27 +0300

    brick - aqueduct circuit swap
    Signed-off-by: SverhRazum-Nah <leon.trol@mail.ru>

--- kubejs/server_scripts/tfc/recipes.js
+++ kubejs/server_scripts/tfc/recipes.js
@@ -1852,7 +1852,7 @@ const registerTFCRecipes = (event) => {
         // Кирпич -> Блок кирпичей
         event.recipes.gtceu.assembler(`bricks_${stone}`)             
             .itemInputs(`5x tfc:brick/${stone}`)
-            .circuit(0)
+            .circuit(1)
             .inputFluids(Fluid.of('gtceu:concrete', 72))
             .itemOutputs(`4x tfc:rock/bricks/${stone}`)
             .duration(50)
@@ -1990,7 +1990,7 @@ const registerTFCRecipes = (event) => {
 
         event.recipes.gtceu.assembler(`aqueduct_${stone}`)             
             .itemInputs(`3x tfc:brick/${stone}`)
-            .circuit(1)
+            .circuit(0)
             .inputFluids(Fluid.of('gtceu:concrete', 16))
             .itemOutputs(`tfc:rock/aqueduct/${stone}`)
             .duration(50)
