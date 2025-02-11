ab45de83df718732df0375b4ba74b700957867ff
Pyritie <Pyritie@gmail.com>
2025-02-11 22:21:11 +0000

    have I fucked up bastions?

--- kubejs/server_scripts/minecraft/tags.js
+++ kubejs/server_scripts/minecraft/tags.js
@@ -27,7 +27,8 @@ const registerMinecraftBlockTags = (event) => {
     event.add('forge:storage_blocks/obsidian', 'minecraft:obsidian')
 
 
-    // Nether lush cave
+    //#region Nether blocks
+
     event.add('minecraft:lush_ground_replaceable', 'minecraft:blackstone')
     event.add('beneath:nether_bush_plantable_on', 'minecraft:blackstone')
     event.add('beneath:nether_bush_plantable_on', 'minecraft:deepslate')
@@ -46,4 +47,46 @@ const registerMinecraftBlockTags = (event) => {
     event.add('tfc:creeping_stone_plantable_on', 'minecraft:blackstone')
     event.add('tfc:creeping_stone_plantable_on', 'minecraft:calcite')
     event.add('minecraft:axolotls_spawnable_on', 'minecraft:calcite')
+
+    
+    event.add('tfc:rock/raw', 'minecraft:deepslate')
+    event.add('tfc:can_landslide', 'minecraft:cobbled_deepslate')
+    event.add('tfg:rock_stairs', 'minecraft:cobbled_deepslate_stairs')
+    event.add('tfg:rock_slabs', 'minecraft:cobbled_deepslate_slab')
+    event.add('tfg:rock_walls', 'minecraft:cobbled_deepslate_wall')
+    event.add('tfg:brick_stairs', 'minecraft:deepslate_brick_stairs')
+    event.add('tfg:brick_slabs', 'minecraft:deepslate_brick_slab')
+    event.add('tfg:brick_walls', 'minecraft:deepslate_brick_wall')
+    event.add('tfg:brick_stairs', 'minecraft:deepslate_tile_stairs')
+    event.add('tfg:brick_slabs', 'minecraft:deepslate_tile_slab')
+    event.add('tfg:brick_walls', 'minecraft:deepslate_tile_wall')
+
+    event.remove('tfc:can_collapse', 'minecraft:chiseled_polished_blackstone')
+    event.remove('tfc:can_start_collapse', 'minecraft:chiseled_polished_blackstone')
+    event.remove('tfc:can_trigger_collapse', 'minecraft:chiseled_polished_blackstone')
+    event.remove('tfc:can_collapse', 'minecraft:polished_blackstone_bricks')
+    event.remove('tfc:can_start_collapse', 'minecraft:polished_blackstone_bricks')
+    event.remove('tfc:can_trigger_collapse', 'minecraft:polished_blackstone_bricks')
+    event.remove('tfc:can_collapse', 'minecraft:cracked_polished_blackstone_bricks')
+    event.remove('tfc:can_start_collapse', 'minecraft:cracked_polished_blackstone_bricks')
+    event.remove('tfc:can_trigger_collapse', 'minecraft:cracked_polished_blackstone_bricks')
+    event.remove('tfc:can_collapse', 'minecraft:polished_blackstone_brick_stairs')
+    event.remove('tfc:can_start_collapse', 'minecraft:polished_blackstone_brick_stairs')
+    event.remove('tfc:can_trigger_collapse', 'minecraft:polished_blackstone_brick_stairs')
+    event.remove('tfc:can_collapse', 'minecraft:polished_blackstone_brick_slab')
+    event.remove('tfc:can_start_collapse', 'minecraft:polished_blackstone_brick_slab')
+    event.remove('tfc:can_trigger_collapse', 'minecraft:polished_blackstone_brick_slab')
+    event.remove('tfc:can_collapse', 'minecraft:polished_blackstone_brick_wall')
+    event.remove('tfc:can_start_collapse', 'minecraft:polished_blackstone_brick_wall')
+    event.remove('tfc:can_trigger_collapse', 'minecraft:polished_blackstone_brick_wall')
+
+    event.add('tfc:rock/raw', 'minecraft:blackstone')
+    event.add('tfg:rock_stairs', 'minecraft:blackstone_stairs')
+    event.add('tfg:rock_slabs', 'minecraft:blackstone_slab')
+    event.add('tfg:rock_walls', 'minecraft:blackstone_wall')
+    event.add('tfg:brick_stairs', 'minecraft:blackstone_stairs')
+    event.add('tfg:brick_slabs', 'minecraft:blackstone_slab')
+    event.add('tfg:brick_walls', 'minecraft:blackstone_wall')
+    
+    //#region
 }
