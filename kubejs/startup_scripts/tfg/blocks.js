957d209f8e94f5cc19f44bd1fe4539a06d4c7446
Pyritie <Pyritie@gmail.com>
2025-02-26 00:33:58 +0000

    ash piles, charred tree, biomes, etc etc

--- kubejs/startup_scripts/tfg/blocks.js
+++ kubejs/startup_scripts/tfg/blocks.js
@@ -16,7 +16,8 @@ const registerTFGBlocks = (event) => {
 	// #region Nether blocks
 
 	event.create('tfg:hardened_deepslate')
-		.stoneSoundType()
+		.soundType('deepslate')
+		.property(BlockProperties.AXIS)
 		.requiresTool(true)
 		.item(item => {
 			item.modelJson({ parent: 'minecraft:item/deepslate' })
@@ -58,6 +59,7 @@ const registerTFGBlocks = (event) => {
 		.property(BlockProperties.LAYERS)
 		.sandSoundType()
 		.tagBoth('minecraft:mineable/shovel')
+		.tagBoth('tfc:can_collapse')
 		.hardness(0.5)
 		.mapColor('grey')
 		.speedFactor(0.7)
