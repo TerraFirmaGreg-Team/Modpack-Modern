c07daf29a20b712297320ce10845720f2afe01b2
Pyritie <Pyritie@gmail.com>
2025-02-26 21:50:25 +0000

    more veins!

--- kubejs/startup_scripts/gtceu/tag_prefixes.js
+++ kubejs/startup_scripts/gtceu/tag_prefixes.js
@@ -365,11 +365,21 @@ const registerGTCEuTagPrefixes = (event) => {
 		.materialIconType(GTMaterialIconType.ore)
 		.generationCondition(ItemGenerationCondition.hasOreProperty)
 
-    event.create('dripstone', 'ore')
+
+	event.create('dripstone', 'ore')
 		.stateSupplier(() => Block.getBlock('minecraft:dripstone_block').defaultBlockState())
 		.baseModelLocation('minecraft:block/dripstone_block')
 		.unificationEnabled(true)
         .materialIconType(GTMaterialIconType.ore)
+		.generationCondition(ItemGenerationCondition.hasOreProperty)
+
+    // TODO: Delete this, fix it in tfg-core instead
+
+	event.create('deepslate', 'ore')
+		.stateSupplier(() => Block.getBlock('minecraft:deepslate').defaultBlockState())
+		.baseModelLocation('minecraft:block/deepslate')
+		.unificationEnabled(true)
+		.materialIconType(GTMaterialIconType.ore)
 		.generationCondition(ItemGenerationCondition.hasOreProperty)
 
     /* Tag Fixes */
