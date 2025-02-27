7ba56b7daaa72a612795f4c8e785f4155cda5c21
Pyritie <pyritie@gmail.com>
2025-02-27 12:43:20 +0000

    beneath disabled category (todo)

--- kubejs/server_scripts/beneath/tags.js
+++ kubejs/server_scripts/beneath/tags.js
@@ -1,9 +1,20 @@
 const registerBeneathBlockTags = (event) => {
 
+	global.BENEATH_DISABLED_ITEMS.forEach(item => {
+		event.removeAllTagsFrom(item)
+		event.add('c:hidden_from_recipe_viewers', item)
+	})
+
+
 	event.add('forge:ores', 'beneath:ore/nether_cursecoal')
 }
 
 const registerBeneathItemTags = (event) => {
+
+	global.BENEATH_DISABLED_ITEMS.forEach(item => {
+		event.removeAllTagsFrom(item)
+		event.add('c:hidden_from_recipe_viewers', item)
+	})
 
 	event.add('forge:ores', 'beneath:ore/nether_cursecoal')
 }
