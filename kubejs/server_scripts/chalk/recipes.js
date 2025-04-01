9989eb2ac24c42d77b3fdd13634d6e6a4960583a
Pyritie <Pyritie@gmail.com>
2025-04-02 00:41:54 +0100

    removed small/tiny dust recipes for chalks

--- kubejs/server_scripts/chalk/recipes.js
+++ kubejs/server_scripts/chalk/recipes.js
@@ -7,48 +7,30 @@ const registerChalkRecipes = (evt) => {
 	evt.replaceInput({ id: "chalk:chalk_box" }, "minecraft:slime_ball", "tfc:glue")
 
 	//Mix dusts for chalk sticks with clay to make an unfired chalk stick. Greggy or Create lets you use tiny dusts if needed
-	let sizes = ["tiny_", "small_", "regular_"]
-	let count = [9, 4, 1]
+	evt.recipes.firmalife.mixing_bowl()
+		.itemIngredients(["minecraft:clay_ball", "#chalk:dusts_for_chalks"])
+		.outputItem("tfg:unfired_chalk")
+		.id(`chalk:mixing_bowl/unfired_chalk_stick_from_dust`)
 
-	for (let i = 0; i < 3; i++) {
+	global.MINECRAFT_DYE_NAMES.forEach(dyeName => {
+		//gt mixer works as is
+		evt.recipes.gtceu.chemical_bath(`chalk:gt_mixer/${dyeName}_chalk_from_dust`)
+			.itemInputs(["minecraft:clay_ball", `#chalk:dusts_for_chalks`])
+			.inputFluids([Fluid.of(`tfc:${dyeName}_dye`, 36)])
+			.itemOutputs([`chalk:${dyeName}_chalk`])
+			.duration(600)
+			.EUt(1)
+			.category(GTRecipeCategories.CHEM_DYES);
 
-		var sizeComputed = i < 2 ? sizes[i] : "";
-		var amount = count[i];
+		//create mixer creates the unfired colored stick, unless heated.
+		let createIngredients = ["minecraft:clay_ball", `#chalk:dusts_for_chalks`, Fluid.of(`tfc:${dyeName}_dye`, 36)]
+		evt.recipes.create.mixing(`tfg:wet_${dyeName}_chalk`, createIngredients)
+			.id(`chalk:create_mixer/wet_${dyeName}_chalk_from_dust`);
 
-		if (i != 0) {
-
-			let itemIngredients = new Array("minecraft:clay_ball");
-			for (let j = 0; j < amount; j++) {
-				itemIngredients.push(`#chalk:${sizeComputed}dusts_for_chalks`);
-			}
-
-			evt.recipes.firmalife.mixing_bowl()
-				.itemIngredients(itemIngredients)
-				.outputItem("tfg:unfired_chalk")
-				.id(`chalk:mixing_bowl/unfired_chalk_stick_from_${sizeComputed}dust`)
-		}
-
-		global.MINECRAFT_DYE_NAMES.forEach(dyeName => {
-
-			//gt mixer works as is
-			evt.recipes.gtceu.chemical_bath(`chalk:gt_mixer/${dyeName}_chalk_from_${sizeComputed}dust`)
-				.itemInputs(["minecraft:clay_ball", `${count[i]}x #chalk:${sizeComputed}dusts_for_chalks`])
-				.inputFluids([Fluid.of(`tfc:${dyeName}_dye`, 36)])
-				.itemOutputs([`chalk:${dyeName}_chalk`])
-				.duration(600)
-				.EUt(1)
-				.category(GTRecipeCategories.CHEM_DYES);
-
-			//create mixer creates the unfired colored stick, unless heated.
-			let createIngredients = ["minecraft:clay_ball", `${count[i]}x #chalk:${sizeComputed}dusts_for_chalks`, Fluid.of(`tfc:${dyeName}_dye`, 36)]
-			evt.recipes.create.mixing(`tfg:wet_${dyeName}_chalk`, createIngredients)
-				.id(`chalk:create_mixer/wet_${dyeName}_chalk_from_${sizeComputed}dust`);
-
-			evt.recipes.create.mixing(`chalk:${dyeName}_chalk`, createIngredients)
-				.heated()
-				.id(`chalk:create_mixer/${dyeName}_chalk_from_${sizeComputed}dust`);
-		})
-	}
+		evt.recipes.create.mixing(`chalk:${dyeName}_chalk`, createIngredients)
+			.heated()
+			.id(`chalk:create_mixer/${dyeName}_chalk_from_dust`);
+	})
 
 	//Unfired chalk sticks need to be placed in a barrel full of dye to colorize. Then heated until cured.
 	global.MINECRAFT_DYE_NAMES.forEach(dyeName => {
--- kubejs/server_scripts/chalk/tags.js
+++ kubejs/server_scripts/chalk/tags.js
@@ -4,10 +4,7 @@
  */
 const registerChalkItemTags = (evt) =>
 {
-    ["tiny_", "small_", ""].forEach(suffix =>
-    {
-        evt.add(`chalk:${suffix}dusts_for_chalks`, `tfg:${suffix}chalk_dust`)
-        evt.add(`chalk:${suffix}dusts_for_chalks`, `tfg:${suffix}limestone_dust`)
-        evt.add(`chalk:${suffix}dusts_for_chalks`, `tfg:${suffix}dripstone_dust`)
-    })
+    evt.add(`chalk:dusts_for_chalks`, `tfg:chalk_dust`)
+    evt.add(`chalk:dusts_for_chalks`, `tfg:limestone_dust`)
+    evt.add(`chalk:dusts_for_chalks`, `tfg:dripstone_dust`)
 }
