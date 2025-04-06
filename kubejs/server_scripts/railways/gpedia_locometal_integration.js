e6ee4af72f93779f4f11502eab853ec19f83a4bc
Pyritie <Pyritie@gmail.com>
2025-04-06 17:29:04 +0100

    fix copper/brass plates

--- kubejs/server_scripts/railways/gpedia_locometal_integration.js
+++ kubejs/server_scripts/railways/gpedia_locometal_integration.js
@@ -19,47 +19,47 @@ const locometalBase = {
 }
 
 const railwaysLocometalIntegration = (event) => {
-	for(var locometal in locometalBase) {
-		event.recipes.create.cutting(`8x railways:${locometal}`, 'minecraft:iron_block').processingTime(200);
-		event.recipes.create.cutting(`16x railways:${locometal}`, 'gtceu:wrought_iron_block').processingTime(200);
-		event.recipes.create.cutting(`24x railways:${locometal}`, 'gtceu:steel_block').processingTime(200);
+	for (var locometal in locometalBase) {
+		event.recipes.create.cutting(`8x railways:${locometal}`, '#forge:storage_blocks/iron').processingTime(200);
+		event.recipes.create.cutting(`16x railways:${locometal}`, '#forge:storage_blocks/wrought_iron').processingTime(200);
+		event.recipes.create.cutting(`24x railways:${locometal}`, '#forge:storage_blocks/steel').processingTime(200);
 		event.recipes.create.cutting(`railways:${locometal}`, `#railways:palettes/cycle_groups/base`).processingTime(20);
 		event.recipes.gtceu.chemical_bath(`gpedia:undying/locometal/${locometal}`)
-				.itemInputs(Item.of(locometalDyeGroups[`${locometal}`], 1))
-				.inputFluids(Fluid.of(`gtceu:chlorine`, 18))
-				.itemOutputs(Item.of(`railways:${locometal}`))
-				.duration(20)
-				.EUt(24)
-	}
-	
-	event.recipes.create.item_application(`railways:iron_wrapped_locometal`, [`#railways:palettes/cycle_groups/base`,'gtceu:wrought_iron_plate']);
-	event.recipes.create.item_application(`railways:copper_wrapped_locometal`, [`#railways:palettes/cycle_groups/base`,'gtceu:copper_plate']);
-	event.recipes.create.item_application(`railways:brass_wrapped_locometal`, [`#railways:palettes/cycle_groups/base`,'gtceu:brass_plate'])
-	
-	event.recipes.gtceu.chemical_bath(`gpedia:undying/brass_wrapped_locometal`)
-			.itemInputs('#railways:palettes/dye_groups/brass_wrapped_slashed')
+			.itemInputs(Item.of(locometalDyeGroups[`${locometal}`], 1))
 			.inputFluids(Fluid.of(`gtceu:chlorine`, 18))
-			.itemOutputs(`railways:brass_wrapped_locometal`)
-			.duration(20)
-			.EUt(24);
-		event.recipes.gtceu.chemical_bath(`gpedia:undying/copper_wrapped_locometal`)
-			.itemInputs('#railways:palettes/dye_groups/copper_wrapped_slashed')
-			.inputFluids(Fluid.of(`gtceu:chlorine`, 18))
-			.itemOutputs(`railways:copper_wrapped_locometal`)
-			.duration(20)
-			.EUt(24);
-		event.recipes.gtceu.chemical_bath(`gpedia:undying/iron_wrapped_locometal`)
-			.itemInputs('#railways:palettes/dye_groups/iron_wrapped_slashed')
-			.inputFluids(Fluid.of(`gtceu:chlorine`, 18))
-			.itemOutputs(`railways:iron_wrapped_locometal`)
+			.itemOutputs(Item.of(`railways:${locometal}`))
 			.duration(20)
 			.EUt(24)
+	}
+
+	event.recipes.create.item_application(`railways:iron_wrapped_locometal`, [`#railways:palettes/cycle_groups/base`, '#forge:plates/wrought_iron']);
+	event.recipes.create.item_application(`railways:copper_wrapped_locometal`, [`#railways:palettes/cycle_groups/base`, '#forge:plates/copper']);
+	event.recipes.create.item_application(`railways:brass_wrapped_locometal`, [`#railways:palettes/cycle_groups/base`, '#forge:plates/brass'])
+
+	event.recipes.gtceu.chemical_bath(`gpedia:undying/brass_wrapped_locometal`)
+		.itemInputs('#railways:palettes/dye_groups/brass_wrapped_slashed')
+		.inputFluids(Fluid.of(`gtceu:chlorine`, 18))
+		.itemOutputs(`railways:brass_wrapped_locometal`)
+		.duration(20)
+		.EUt(24);
+	event.recipes.gtceu.chemical_bath(`gpedia:undying/copper_wrapped_locometal`)
+		.itemInputs('#railways:palettes/dye_groups/copper_wrapped_slashed')
+		.inputFluids(Fluid.of(`gtceu:chlorine`, 18))
+		.itemOutputs(`railways:copper_wrapped_locometal`)
+		.duration(20)
+		.EUt(24);
+	event.recipes.gtceu.chemical_bath(`gpedia:undying/iron_wrapped_locometal`)
+		.itemInputs('#railways:palettes/dye_groups/iron_wrapped_slashed')
+		.inputFluids(Fluid.of(`gtceu:chlorine`, 18))
+		.itemOutputs(`railways:iron_wrapped_locometal`)
+		.duration(20)
+		.EUt(24)
 
 	global.MINECRAFT_DYE_NAMES.forEach(dye => {
-		for(var locometal in locometalBase) {
+		for (var locometal in locometalBase) {
 			event.recipes.gtceu.chemical_bath(`gpedia:chemical_dying_locometal/${locometal}/${dye}`)
 				.itemInputs(Item.of(locometalDyeGroups[`${locometal}`], 1))
-				.inputFluids(Fluid.of(`gtceu:${dye}_dye`, 18))
+				.inputFluids(Fluid.of(`tfc:${dye}_dye`, 18))
 				.itemOutputs(Item.of(`railways:${dye}_${locometal}`))
 				.duration(20)
 				.EUt(24);
@@ -71,31 +71,26 @@ const railwaysLocometalIntegration = (event) => {
 				]
 			)
 		};
-		event.recipes.create.item_application(`railways:${dye}_iron_wrapped_locometal`, [`#railways:palettes/cycle_groups/${dye}`, 'gtceu:wrought_iron_plate']);
-		event.recipes.create.item_application(`railways:${dye}_copper_wrapped_locometal`, [`#railways:palettes/cycle_groups/${dye}`, 'gtceu:copper_plate']);
-		event.recipes.create.item_application(`railways:${dye}_brass_wrapped_locometal`, [`#railways:palettes/cycle_groups/${dye}`, 'gtceu:brass_plate']);
+		event.recipes.create.item_application(`railways:${dye}_iron_wrapped_locometal`, [`#railways:palettes/cycle_groups/${dye}`, '#forge:plates/wrought_iron']);
+		event.recipes.create.item_application(`railways:${dye}_copper_wrapped_locometal`, [`#railways:palettes/cycle_groups/${dye}`, '#forge:plates/copper']);
+		event.recipes.create.item_application(`railways:${dye}_brass_wrapped_locometal`, [`#railways:palettes/cycle_groups/${dye}`, '#forge:plates/brass']);
 		event.recipes.gtceu.chemical_bath(`gpedia:brass_locometal_bathing/${dye}`)
 			.itemInputs('#railways:palettes/dye_groups/brass_wrapped_slashed')
-			.inputFluids(Fluid.of(`gtceu:${dye}_dye`, 18))
+			.inputFluids(Fluid.of(`tfc:${dye}_dye`, 18))
 			.itemOutputs(`railways:${dye}_brass_wrapped_locometal`)
 			.duration(20)
 			.EUt(24);
 		event.recipes.gtceu.chemical_bath(`gpedia:copper_locometal_bathing/${dye}`)
 			.itemInputs('#railways:palettes/dye_groups/copper_wrapped_slashed')
-			.inputFluids(Fluid.of(`gtceu:${dye}_dye`, 18))
+			.inputFluids(Fluid.of(`tfc:${dye}_dye`, 18))
 			.itemOutputs(`railways:${dye}_copper_wrapped_locometal`)
 			.duration(20)
 			.EUt(24);
 		event.recipes.gtceu.chemical_bath(`gpedia:locometal_bathing/${dye}`)
 			.itemInputs('#railways:palettes/dye_groups/iron_wrapped_slashed')
-			.inputFluids(Fluid.of(`gtceu:${dye}_dye`, 18))
+			.inputFluids(Fluid.of(`tfc:${dye}_dye`, 18))
 			.itemOutputs(`railways:${dye}_iron_wrapped_locometal`)
 			.duration(20)
 			.EUt(24);
 	})
-	
 }
-
-ServerEvents.recipes( event => {
-	railwaysLocometalIntegration(event)
-})
