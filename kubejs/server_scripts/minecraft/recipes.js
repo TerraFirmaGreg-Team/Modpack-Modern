56311bef5b6dadfd12a0f5d787a9e0fcf4b7bd82
Pyritie <pyritie@gmail.com>
2025-04-16 18:42:16 +0100

    Merges #654, getting leather back from leather things

--- kubejs/server_scripts/minecraft/recipes.js
+++ kubejs/server_scripts/minecraft/recipes.js
@@ -864,4 +864,36 @@ const registerMinecraftRecipes = (event) => {
 	).id('tfg:shapeless/calcite_from_rich_raw')
 
 	// #endregion
+
+	//#region Кожа из кожаных предметов
+	event.recipes.gtceu.macerator('tfg:leather_from_boots')
+		.itemInputs('minecraft:leather_boots')
+		.itemOutputs('minecraft:leather')
+		.EUt(7).duration(80)
+
+	event.recipes.gtceu.macerator('tfg:leather_from_saddle')
+		.itemInputs('minecraft:saddle')
+		.itemOutputs('minecraft:leather')
+		.EUt(7).duration(80)
+
+	event.recipes.gtceu.macerator('tfg:leather_from_horse_armor')
+		.itemInputs('minecraft:leather_horse_armor')
+		.itemOutputs('minecraft:leather')
+		.EUt(7).duration(80)
+
+	event.recipes.gtceu.macerator('tfg:leather_from_helmet')
+		.itemInputs('minecraft:leather_helmet')
+		.itemOutputs('minecraft:leather')
+		.EUt(7).duration(80)
+
+	event.recipes.gtceu.macerator('tfg:leather_from_leggings')
+		.itemInputs('minecraft:leather_leggings')
+		.itemOutputs('minecraft:leather')
+		.EUt(7).duration(80)
+
+	event.recipes.gtceu.macerator('tfg:leather_from_chestplate')
+		.itemInputs('minecraft:leather_chestplate')
+		.itemOutputs('minecraft:leather')
+		.EUt(7).duration(80)
+	//#endregion
 }
