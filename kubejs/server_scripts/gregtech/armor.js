41c6f6bae6de9434075364ca3a08fe50fba3320b
Pyritie <pyritie@gmail.com>
2025-04-22 12:54:39 +0100

    Update armor.js
    Signed-off-by: Pyritie <pyritie@gmail.com>

--- kubejs/server_scripts/gregtech/armor.js
+++ kubejs/server_scripts/gregtech/armor.js
@@ -1,12 +1,9 @@
-const INFINTE = Java.loadClass('java.lang.Integer').MAX_VALUE;
 PlayerEvents.tick((event) => {
-  const { player } = event;
-  if (player.age % 20 != 0) return;
-  if (player.legsArmorItem == 'gtceu:nanomuscle_leggings') {
-    player.potionEffects.add("minecraft:speed", INFINTE, 1, true, false);
-  } else {
-    player.removeEffect("minecraft:speed");
-  }
+    const { player } = event;
+    if (player.age % 100 != 0) return;
+    if (player.legsArmorItem == 'gtceu:nanomuscle_leggings') {
+        player.potionEffects.add("minecraft:speed", 350, 1, true, false);
+    } else {
+        player.removeEffect("minecraft:speed");
+    }
 });
-
-
