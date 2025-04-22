90bd1a9237ac39c44a5023848cedb02feb2c6a85
TomPlop <tomdidome@gmail.com>
2025-04-22 19:43:43 -0400

    Fixes for scaffolding frame and bloomery steam consumption so it works with only one High pressure liquid boiler

--- kubejs/server_scripts/tfc/recipes.js
+++ kubejs/server_scripts/tfc/recipes.js
@@ -234,4 +234,5 @@ const registerTFCRecipes = (event) => {
 	// Brass Mechanism
 	event.recipes.shapeless('gtceu:small_brass_gear', [ 'tfc:brass_mechanisms' ]).id('tfg:replace_brass_mechanisms')
 	event.replaceInput({ input: 'tfc:brass_mechanisms' }, 'tfc:brass_mechanisms', 'gtceu:small_brass_gear')
+
 }
