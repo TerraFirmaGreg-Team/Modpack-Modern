6b52c640d3f9263aae7881336400f18c65dde70d
TomPlop <tomdidome@gmail.com>
2025-04-24 22:59:22 -0400

    Fix for Universal Circuits recipes

--- kubejs/server_scripts/tfg/universal_circuit.js
+++ kubejs/server_scripts/tfg/universal_circuit.js
@@ -1,25 +1,14 @@
-const tiers = ["ulv", "lv", "mv", "hv", "ev", "iv", "luv", "zpm", "uv", "uhv", "uev","uiv"];
+const tiers = ["ulv", "lv", "mv", "hv", "ev", "iv", "luv", "zpm", "uv", "uhv"];
 
 ServerEvents.tags("item", event => {
-	tiers.forEach(tier => {
-		event.add(`kubejs:craft_circuits/${tier}`, `#gtceu:circuits/${tier}`);
-	});
+    tiers.forEach(tier => {
+        event.add(`gtceu:circuits/${tier}`, `tfg:${tier}_universal_circuit`);
+    });
 });
 
 ServerEvents.recipes(event => {
 	tiers.forEach(tier => {
-		event.shapeless(
-            Item.of(`tfg:${tier}_universal_circuit`, 1),
-            [
-              `kubejs:craft_circuits_${tier}`
-            ]
-          );
-	});
-
+	event.shapeless(Item.of(`tfg:${tier}_universal_circuit`, 1),  [Ingredient.of([`#gtceu:circuits/${tier}`]).subtract([`tfg:${tier}_universal_circuit`])]
+		).id(`universal_circuits_${tier}`);
 });
-
-ServerEvents.tags("item", event => {
-	tiers.forEach(tier => {
-		event.add(`gtceu:circuits/${tier}`, `tfg:${tier}_universal_circuit`);
-	});
 });
