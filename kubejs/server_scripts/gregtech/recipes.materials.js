1d9cb7232c3c70e6aa23f964d1e77bc1474c46e0
oosyrag <oosyrag@gmail.com>
2025-05-16 10:18:21 -0700

    corrections

--- kubejs/server_scripts/gregtech/recipes.materials.js
+++ kubejs/server_scripts/gregtech/recipes.materials.js
@@ -114,17 +114,17 @@ function registerGTCEUMetalRecipes(event) {
 
 		if (usableItem.isEmpty()) return
 
-		event.recipes.tfc.damage_inputs_shaped_crafting(
+//		event.recipes.tfc.damage_inputs_shaped_crafting(
 			event.shaped(toolItem, [
-				'CA ',
-				' B ',
-				'   '
+				'CA',
+				' B'
 			], {
 				A: usableItem,
 				B: '#tfc:rock/raw',
 				C: '#tfc:chisels'
 			})
-		).id(`gtceu:shaped/mortar_${material.getName()}`)
+//		)
+		.id(`gtceu:shaped/mortar_${material.getName()}`)
 	}
 
 	const processIngot = (material) => {
--- kubejs/server_scripts/tfg/recipes.js
+++ kubejs/server_scripts/tfg/recipes.js
@@ -14,29 +14,4 @@ const registerTFGRecipes = (event) => {
 	registerTFGSupportRecipes(event)
 	registerTFGFishingNetsRecipes(event)
 	registerTFGConvertingRecipes(event)
-
-	//Fixes for 2x2 crafts crashing when auto inputting to crafting grid via JEI by padding recipes to 3x3 with empty space.
-	//https://github.com/emilyploszaj/emi/issues/942
-
-	event.shaped('waterflasks:iron_flask', [
-		'AB ',
-		'CD ',
-		'   '
-	], {
-		A: 'waterflasks:iron_flask',
-		B: 'waterflasks:bladder',
-		C: 'tfc:burlap_cloth',
-		D: '#forge:tools/knives'
-	}).id('waterflasks:crafting/repair_iron');
-
-	event.shaped('waterflasks:iron_flask', [
-		'AB ',
-		'CD ',
-		'   '
-	], {
-		A: 'waterflasks:broken_iron_flask',
-		B: 'waterflasks:bladder',
-		C: 'tfc:burlap_cloth',
-		D: '#forge:tools/knives'
-	}).id('waterflasks:crafting/repair_broken_iron');
 }
