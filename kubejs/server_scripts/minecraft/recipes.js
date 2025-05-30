4c32cbbccb7b1b83e91ab633e6222d30fa60c3df
TomPlop <tomdidome@gmail.com>
2025-05-30 14:56:32 -0400

    Fix for Hopper conflict with Assembler

--- kubejs/server_scripts/minecraft/recipes.js
+++ kubejs/server_scripts/minecraft/recipes.js
@@ -597,6 +597,7 @@ const registerMinecraftRecipes = (event) => {
 	event.recipes.gtceu.assembler('hopper_wrought_iron')
 		.itemInputs('#forge:chests', '5x #forge:plates/wrought_iron')
 		.itemOutputs('minecraft:hopper')
+		.circuit(8)
 		.duration(700)
 		.EUt(2)
 
