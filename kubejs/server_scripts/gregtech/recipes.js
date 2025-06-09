ecfe925a5feab5a9f6efb85d88b0f7497a4a98ae
GameStar <56610486+BlueBoat29@users.noreply.github.com>
2025-06-08 20:31:12 -0500

    updated tag to tfg from ae2

--- kubejs/server_scripts/gregtech/recipes.js
+++ kubejs/server_scripts/gregtech/recipes.js
@@ -1134,10 +1134,10 @@ const registerGTCEURecipes = (event) => {
 	//#endregion
 	
 	//#region GT Facades
-	event.shapeless(Item.of('gtceu:facade_cover', 8, '{Facade: {Count:1b,id:"minecraft:stone"}}'), ['3x #forge:plates/iron', "#ae2:whitelisted/facades"])
+	event.shapeless(Item.of('gtceu:facade_cover', 8, '{Facade: {Count:1b,id:"minecraft:stone"}}'), ['3x #forge:plates/iron', "#tfg:whitelisted/facades"])
     .modifyResult((craftingGrid, result) =>
     {
-       let blockID = craftingGrid.find(Ingredient.of("#ae2:whitelisted/facades")).id
+       let blockID = craftingGrid.find(Ingredient.of("#tfg:whitelisted/facades")).id
 	   
 		console.log(blockID)
 		let facadeNBT = "{Facade: {Count:1b,id:" + "'" + blockID + "'" + "}}"
@@ -1145,10 +1145,10 @@ const registerGTCEURecipes = (event) => {
         return result;
     }).id('gtceu:facade_cover');
 	
-	event.shapeless(Item.of('gtceu:facade_cover', 32, '{Facade: {Count:1b,id:"minecraft:stone"}}'), ['3x #forge:plates/titanium', "#ae2:whitelisted/facades"])
+	event.shapeless(Item.of('gtceu:facade_cover', 32, '{Facade: {Count:1b,id:"minecraft:stone"}}'), ['3x #forge:plates/titanium', "#tfg:whitelisted/facades"])
     .modifyResult((craftingGrid, result) =>
     {
-       let blockID = craftingGrid.find(Ingredient.of("#ae2:whitelisted/facades")).id
+       let blockID = craftingGrid.find(Ingredient.of("#tfg:whitelisted/facades")).id
 	   
 		console.log(blockID)
 		let facadeNBT = "{Facade: {Count:1b,id:" + "'" + blockID + "'" + "}}"
