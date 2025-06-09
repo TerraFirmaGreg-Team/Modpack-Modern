2b3a1d8ee2baf2b8b1b77c43756c334bee071a96
Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>
2025-06-09 15:34:05 -0300

    Conductor (#1144)
    * Remove Scaffolding Frame recipe
    
    Signed-off-by: Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>
    
    * Update Scaffolding Frame recipe
    
    Signed-off-by: Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>
    
    * Fix Glass Jar conflict 
    
    Signed-off-by: Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>
    
    * Conductor Tags
    
    Signed-off-by: Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>
    
    * Conductor Recipes
    
    Signed-off-by: Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>
    
    * Update constants.js
    
    Signed-off-by: Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>
    
    * Update recipes.js
    
    Signed-off-by: Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>
    
    * update id
    
    Signed-off-by: Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>

--- kubejs/server_scripts/railways/tags.js
+++ kubejs/server_scripts/railways/tags.js
@@ -7,8 +7,12 @@ const registerRailWaysItemTags = (event) => {
         event.removeAllTagsFrom(item)
         event.add('c:hidden_from_recipe_viewers', item)
     })
+    // Adding Conductor tags
+    global.MINECRAFT_DYE_NAMES.forEach(dye => {
+		if (dye != 'white') event.add('tfg:colored_caps', `railways:${dye}_conductor_cap`)
+		event.remove('c:hidden_from_recipe_viewers',`railways:${dye}_conductor_cap`)
+	})
 }
-
 const registerRailWaysBlockTags = (event) => {
     
     // Удаление тегов у отключенных предметов
