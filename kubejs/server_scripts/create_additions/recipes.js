8c21614206d7fe0ed9fde5f76c0ba4ce54998534
34y34y34y34y34y34y34y34y34y <34y34y34y34y34y34y34y34y34y@gmail.com>
2025-06-21 16:31:07 +0300

    Tier 5 now for straw
    Tom edited the message after I saw it .
    
    Signed-off-by: 34y34y34y34y34y34y34y34y34y <34y34y34y34y34y34y34y34y34y@gmail.com>

--- kubejs/server_scripts/create_additions/recipes.js
+++ kubejs/server_scripts/create_additions/recipes.js
@@ -96,7 +96,7 @@ const registerCreateAdditionsRecipes = (event) => {
 //#region Liquid blaze burners
 	
 	event.recipes.tfc.anvil('createaddition:straw', '#forge:small_fluid_pipes/tin_alloy', ['shrink_second_last', 'upset_not_last', 'punch_last'])
-        	.tier(3)
+        	.tier(5)
         	.id(`tfc:anvil/straw`)
 
     	event.recipes.gtceu.lathe('assembler:createaddition_straw')
