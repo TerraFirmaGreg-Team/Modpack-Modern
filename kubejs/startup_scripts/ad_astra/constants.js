9107d124a8f914181a75081e355411e7a4a28bc6
Pyritie <pyritie@gmail.com>
2025-07-01 01:12:38 +0100

    cheese

--- kubejs/startup_scripts/ad_astra/constants.js
+++ kubejs/startup_scripts/ad_astra/constants.js
@@ -18,7 +18,6 @@ global.AD_ASTRA_DISABLED_ITEMS =
 	'ad_astra:water_pump',
 	'ad_astra:cryo_freezer',
 	'ad_astra:wrench',
-	'ad_astra:cheese',
 	'ad_astra:cheese_block',
 	'ad_astra:photovoltaic_etrium_cell',
 	'ad_astra:photovoltaic_vesnium_cell',
