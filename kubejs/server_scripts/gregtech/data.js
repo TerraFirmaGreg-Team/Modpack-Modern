edd59f502ed7b31ad39e6baf3e6ffe96207aee2c
Pyritie <pyritie@gmail.com>
2025-07-06 17:12:09 +0100

    moon bedrock veins

--- kubejs/server_scripts/gregtech/data.js
+++ kubejs/server_scripts/gregtech/data.js
@@ -190,11 +190,33 @@ function registerGTCEUBedrockFluidVeins(event) {
 	event.add('tfg:moon_helium_3', vein => {
 		vein.dimensions('ad_astra:moon')
 		vein.fluid(() => Fluid.of('gtceu:helium_3').fluid)
-		vein.weight(1000)
+		vein.weight(100)
 		vein.minimumYield(200)
 		vein.maximumYield(400)
 		vein.depletionAmount(1)
 		vein.depletionChance(1)
 		vein.depletedYield(200)
+	})
+
+	event.add('tfg:moon_helium', vein => {
+		vein.dimensions('ad_astra:moon')
+		vein.fluid(() => Fluid.of('gtceu:helium').fluid)
+		vein.weight(10)
+		vein.minimumYield(50)
+		vein.maximumYield(20)
+		vein.depletionAmount(1)
+		vein.depletionChance(1)
+		vein.depletedYield(50)
+	})
+
+	event.add('tfg:moon_argon', vein => {
+		vein.dimensions('ad_astra:moon')
+		vein.fluid(() => Fluid.of('gtceu:argon').fluid)
+		vein.weight(5)
+		vein.minimumYield(30)
+		vein.maximumYield(10)
+		vein.depletionAmount(1)
+		vein.depletionChance(1)
+		vein.depletedYield(10)
 	})
 }
