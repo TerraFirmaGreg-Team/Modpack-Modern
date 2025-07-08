774f94c88ffb8c3cbb61a0c64645e58aa463ff60
Pyritie <pyritie@gmail.com>
2025-07-08 17:34:34 +0100

    added programmed circuit card

---
+++ kubejs/server_scripts/pccard/recipes.js
@@ -0,0 +1,7 @@
+// priority: 0
+
+function registerProgrammedCircuitCardRecipes(event) {
+
+	event.replaceInput({ id: 'pccard:item/card_programmed_circuit' }, 'gtceu:programmed_circuit', '#gtceu:circuits/lv')
+
+}
