671d2af43ed8a6ee6848228b46d92aad53f9a650
TomPlop <tomdidome@gmail.com>
2025-07-13 18:53:25 -0400

    Changing input for notConsumable (#1307)

--- kubejs/server_scripts/betterend/recipes.js
+++ kubejs/server_scripts/betterend/recipes.js
@@ -7,21 +7,21 @@ function registerBetterEndRecipes(event) {
 		const recipeId = `betterend:greenhouse_${itemId.replace(':', '_')}`;
 
 		event.recipes.gtceu.greenhouse(recipeId)
-			.itemInputs(element.id)
+			.notConsumable(element.id)
 			.itemOutputs(`8x ${element.id}`)
-			.chancedOutput(element.id, 7500, 1000)
-			.chancedOutput(element.id, 5000, 1000)
+			.chancedOutput(element.id, 750, 0)
+			.chancedOutput(element.id, 500, 0)
 			.duration(36000) // 30 mins
 			.circuit(1)
 			.EUt(GTValues.VA[GTValues.MV])
 			.dimension('ad_astra:moon')
 
 		event.recipes.gtceu.greenhouse(recipeId + "_helium")
-			.itemInputs(element.id)
+			.notConsumable(element.id)
 			.inputFluids(Fluid.of('gtceu:helium_3', 500))
 			.itemOutputs(`8x ${element.id}`)
-			.chancedOutput(element.id, 8500, 1000)
-			.chancedOutput(element.id, 6000, 1000)
+			.chancedOutput(element.id, 4000, 0)
+			.chancedOutput(element.id, 3000, 0)
 			.duration(12000) // 30 mins
 			.circuit(2)
 			.EUt(GTValues.VA[GTValues.MV])
