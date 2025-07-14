18aa4470fc5f60d76e26464d1850833e64a8a21f
GameStar <56610486+BlueBoat29@users.noreply.github.com>
2025-07-13 22:22:46 -0500

    fixed afc wood stomping barrel recipes
    Signed-off-by: GameStar <56610486+BlueBoat29@users.noreply.github.com>

--- kubejs/server_scripts/afc/recipes.js
+++ kubejs/server_scripts/afc/recipes.js
@@ -64,6 +64,20 @@ const registerAFCRecipes = (event) => {
 		event.remove({ id: `afc:crafting/wood/${wood}_button` })
 
 		generateCutterRecipe(event, `afc:wood/planks/${wood}_pressure_plate`, `6x afc:wood/planks/${wood}_button`, 50, 2, `${wood}_button`)
+
+		//Stomping Barrel
+		event.remove({ id: `afc:crafting/wood/${wood}_stomping_barrel`})
+		
+		event.shaped(`afc:wood/stomping_barrel/${wood}`, [
+			'ABA',
+			'AAA',
+			'BBB'
+		], {
+			A: `afc:wood/lumber/${wood}`,
+			B: 'tfc:glue'
+			
+		}).id(`afc:crafting/wood/${wood}_stomping_barrel`)
+		
 	})
 
 	// #endregion
