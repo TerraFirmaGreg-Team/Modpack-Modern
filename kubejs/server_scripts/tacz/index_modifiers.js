074f8ec14f64cc243180ccd6c4f28228193a3478
GameStar <56610486+BlueBoat29@users.noreply.github.com>
2025-07-31 15:20:47 -0500

    stuff

--- kubejs/server_scripts/tacz/index_modifiers.js
+++ kubejs/server_scripts/tacz/index_modifiers.js
@@ -8,29 +8,29 @@
 
 const ammoIndexLogic = (event) => {
 	const id = event.getId().toString();
-/*
+
 	if (!global.TACZ_ENABLED_AMMO.includes(id)) {
 		
 		return event.removeAmmo();
-	}*/
+	}
 }
 
 const attachmentIndexLogic = (event) => {
 	const id = event.getId().toString();	
-	/*
+	
 	if (!global.TACZ_ENABLED_ATTACH.includes(id)) {
 		
 		return event.removeAttachment();
-	}*/
+	}
 }
 
 const gunIndexLogic = (event) => {
 	const id = event.getId().toString();
-	/*
+	
 	if (!global.TACZ_ENABLED_GUNS.includes(id)) {
 		
 		return event.removeGun();
-	}*/
+	}
 }
 
 const attachmentDataLogic = (event) => {
