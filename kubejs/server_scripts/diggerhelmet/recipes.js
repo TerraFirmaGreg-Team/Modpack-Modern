3df6032030284225ad7ace33ff7a3dc2c8492b97
Pyritie <pyritie@gmail.com>
2025-08-05 14:13:43 +0100

    fix silk lining being hidden, fix crafting recipe consuming the needle

--- kubejs/server_scripts/diggerhelmet/recipes.js
+++ kubejs/server_scripts/diggerhelmet/recipes.js
@@ -46,6 +46,7 @@ function registerDiggerHelmetRecipes(event) {
 		C: '#forge:foils/rubber'
 	}).id('tfg:shaped/auto_drink_modifier_rubber')
 
-	event.shapeless('diggerhelmet:silk_lining', ['tfcambiental:silk_cowl', '#forge:string', 'tfc:bone_needle'])
-		.id('tfg:shapeless/diggerhelmet/silk_lining')
+	event.recipes.tfc.damage_inputs_shapeless_crafting(
+		event.shapeless('diggerhelmet:silk_lining', ['tfcambiental:silk_cowl', '#forge:string', 'tfc:bone_needle'])
+	).id('tfg:shapeless/diggerhelmet/silk_lining')
 }
