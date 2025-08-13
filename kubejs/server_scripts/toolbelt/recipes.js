8787781f2777f6c76814b6ca32daf461cc5dc698
Pyritie <pyritie@gmail.com>
2025-08-13 21:29:56 +0100

    this doesn't fix dyed toolbelts (#368), but it does fix the other long-standing issue with upgrading them

--- kubejs/server_scripts/toolbelt/recipes.js
+++ kubejs/server_scripts/toolbelt/recipes.js
@@ -14,7 +14,7 @@ const registerToolBeltRecipes = (event) => {
     ], {
         A: '#forge:string',
         B: '#forge:leather',
-        C: '#forge:plates/wrought_iron'
+        C: '#forge:bolts/rose_gold'
     }).id('tfg:toolbelt/shaped/pouch')
 
     // ToolBelt с размером 2
@@ -26,13 +26,28 @@ const registerToolBeltRecipes = (event) => {
         A: '#forge:string',
         B: '#forge:leather',
         C: '#forge:plates/wrought_iron'
-    }).id(`tfg:toolbelt/shaped/belt_${  2}`)
+    }).id(`tfg:toolbelt/shaped/belt_${2}`)
+
+    event.shapeless(Item.of('toolbelt:belt', {Size:3}), [
+        Ingredient.of(Item.of('toolbelt:belt').strongNBT()),
+        'toolbelt:pouch'
+    ]).id(`tfg:toolbelt/shaped/first_upgrade`)
+
+    event.shapeless(Item.of('toolbelt:belt', {Size:3,Items:[]}), [
+        Ingredient.of(Item.of('toolbelt:belt', {Items:[]}).strongNBT()),
+        'toolbelt:pouch'
+    ]).id(`tfg:toolbelt/shaped/first_upgrade_alt`)
 
     // ToolBelt с размером 3 - 9
     for (let i = 3; i < 10; i++) {
         event.shapeless(Item.of('toolbelt:belt', {Size:i}), [
-            Ingredient.of(Item.of('toolbelt:belt', {Size:i-1}).strongNBT()).or(Ingredient.of(Item.of('toolbelt:belt', {Size:i-1,Items:[]}).strongNBT())),
+            Ingredient.of(Item.of('toolbelt:belt', {Size:i-1}).strongNBT()),
             'toolbelt:pouch'
-        ]).id(`tfg:toolbelt/shaped/belt_${  i}`)
+        ]).id(`tfg:toolbelt/shaped/belt_${i}`)
+
+        event.shapeless(Item.of('toolbelt:belt', {Size:i}), [
+            Ingredient.of(Item.of('toolbelt:belt', {Size:i-1,Items:[]}).strongNBT()),
+            'toolbelt:pouch'
+        ]).id(`tfg:toolbelt/shaped/belt_${i}_alt`)
     }
 }
