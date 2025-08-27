934aa209d9e30ace72b365639db623c9f15c1258
GameStar <56610486+BlueBoat29@users.noreply.github.com>
2025-08-27 18:18:49 -0500

    some container changes  (#1716)
    * stuff
    
    * Create wraptor.json
    
    * Update tags.js
    
    * fix wool
    
    * eggs
    
    * Update constants.js
    
    * more recipe stuff
    
    * doing tooltips the correct way
    
    * oops
    
    Signed-off-by: GameStar <56610486+BlueBoat29@users.noreply.github.com>
    
    * oops
    
    * eater mixin stuff
    
    * add sandworm+dep to pakku
    
    * new configs I think
    
    * soulbind recipes
    
    * thumper
    
    * Update main_server_script.js
    
    * Update pakku-lock.json
    
    * sack configs
    
    * Update sns-server.toml
    
    * Update sns-server.toml
    
    * sack recipes and stuff
    
    * Create soulbinding-common.toml
    
    * soulbind fix
    
    * Update pakku-lock.json
    
    Signed-off-by: GameStar <56610486+BlueBoat29@users.noreply.github.com>
    
    * Update recipes.js
    
    * Update recipes.js
    
    * Delete EffekseerNativeForJava.dll
    
    Signed-off-by: GameStar <56610486+BlueBoat29@users.noreply.github.com>
    
    * Update .gitignore
    
    * sack tags
    
    * sewing table change
    
    * Update tags.js
    
    * straw knapping
    
    * Update CHANGELOG.md
    
    Signed-off-by: GameStar <56610486+BlueBoat29@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: GameStar <56610486+BlueBoat29@users.noreply.github.com>

--- kubejs/server_scripts/sacksnstuff/tags.js
+++ kubejs/server_scripts/sacksnstuff/tags.js
@@ -7,5 +7,21 @@ function registeSNSItemTags(event) {
 		event.removeAllTagsFrom(item)
 		event.add('c:hidden_from_recipe_viewers', item)
 	})
-
+	
+	event.add('sns:allowed_in_ore_sack', 'tfc:kaolin_clay')
+	event.add('sns:allowed_in_ore_sack', 'minecraft:clay_ball')
+	event.add('sns:allowed_in_ore_sack', 'beneath:cursecoal')
+	event.add('sns:allowed_in_ore_sack', 'minecraft:flint')
+	
+	event.add('sns:allowed_in_ore_sack', '#tfc:powders')
+	event.add('sns:allowed_in_ore_sack', '#minecraft:coals')
+	event.add('sns:allowed_in_ore_sack', '#tfg:stone_dusts')
+	event.add('sns:allowed_in_ore_sack', '#forge:rich_raw_materials')
+	event.add('sns:allowed_in_ore_sack', '#forge:raw_materials')
+	event.add('sns:allowed_in_ore_sack', '#forge:poor_raw_materials')
+	
+	event.add('sns:allowed_in_ore_sack', '#tfc:sedimentary_rock')
+	event.add('sns:allowed_in_ore_sack', '#tfc:metamorphic_rock')
+	event.add('sns:allowed_in_ore_sack', '#tfg:igneous_intrusive_rock')
+	event.add('sns:allowed_in_ore_sack', '#tfc:igneous_extrusive_rock')
 }
