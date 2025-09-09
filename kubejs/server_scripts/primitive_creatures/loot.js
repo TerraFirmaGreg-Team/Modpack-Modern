a273790245ea27c8717252e052f6a6b09c2c9c0e
Pyritie <pyritie@gmail.com>
2025-09-09 21:20:20 +0100

    primitive creatures update!

--- kubejs/server_scripts/primitive_creatures/loot.js
+++ kubejs/server_scripts/primitive_creatures/loot.js
@@ -34,10 +34,10 @@ function registerPrimitiveCreaturesLoots(event) {
 			Item.of('tfc:food/strawberry', 4),
 			Item.of('tfc:food/wintergreen_berry', 5)])
 		.addAlternativesLoot(
-			LootEntry.of('primitive_creatures:grh').when(c => c.randomChance(0.8)),
-			LootEntry.of('gtceu:stone_axe').when(c => c.randomChance(0.3)),
+			LootEntry.of('sns:straw_basket').when(c => c.randomChance(0.3)),
 			LootEntry.of('primitive_creatures:totem_0').when(c => c.randomChance(0.1)),
-			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)))
+			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)),
+			LootEntry.of('primitive_creatures:grh'))
 	event.addEntityLootModifier('primitive_creatures:iloger_1')
 		.randomChance(CLOTHING_DROP_RATE)
 		.addWeightedLoot([
@@ -53,9 +53,9 @@ function registerPrimitiveCreaturesLoots(event) {
 		.addAlternativesLoot(
 			LootEntry.of('tfc:plant/field_horsetail').when(c => c.randomChance(0.3)),
 			LootEntry.of('tfc:plant/foxglove').when(c => c.randomChance(0.5)),
-			LootEntry.of('firmalife:beeswax').when(c => c.randomChance(0.8)),
 			LootEntry.of('primitive_creatures:totem_0').when(c => c.randomChance(0.1)),
-			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)))
+			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)),
+			LootEntry.of('firmalife:beeswax'))
 	event.addEntityLootModifier('primitive_creatures:iloger_2')
 		.randomChance(CLOTHING_DROP_RATE)
 		.addWeightedLoot([
@@ -72,7 +72,7 @@ function registerPrimitiveCreaturesLoots(event) {
 		.addAlternativesLoot(
 			LootEntry.of('gtceu:stone_hammer').when(c => c.randomChance(0.2)),
 			LootEntry.of('minecraft:skeleton_skull').when(c => c.randomChance(0.1)),
-			LootEntry.of('primitive_creatures:grh').when(c => c.randomChance(0.5))) // battered wool
+			LootEntry.of('primitive_creatures:grh')) // battered wool
 	event.addEntityLootModifier('primitive_creatures:iloger_3')
 		.randomChance(CLOTHING_DROP_RATE)
 		.addWeightedLoot([
@@ -87,9 +87,9 @@ function registerPrimitiveCreaturesLoots(event) {
 		.addWeightedLoot([1,3], LootEntry.of('minecraft:tipped_arrow')).addPotion("poison")
 		.addAlternativesLoot(
 			LootEntry.of('firmalife:food/nightshade_berry').when(c => c.randomChance(0.7)),
-			LootEntry.of('minecraft:spider_eye').when(c => c.randomChance(0.7)),
 			LootEntry.of('primitive_creatures:totem_0').when(c => c.randomChance(0.1)),
-			LootEntry.of('primitive_creatures:totem_2').when(c => c.randomChance(0.1)))
+			LootEntry.of('primitive_creatures:totem_2').when(c => c.randomChance(0.1)),
+			LootEntry.of('sns:quiver'))
 	event.addEntityLootModifier('primitive_creatures:iloger_4')
 		.randomChance(CLOTHING_DROP_RATE)
 		.addWeightedLoot([
@@ -104,10 +104,10 @@ function registerPrimitiveCreaturesLoots(event) {
 		.addWeightedLoot([1,3], LootEntry.of('minecraft:tipped_arrow')).addPotion("weakness")
 		.addAlternativesLoot(
 			// placeholder for a blowpipe
-			LootEntry.of('minecraft:bamboo').when(c => c.randomChance(0.6)),
-			LootEntry.of('primitive_creatures:grh').when(c => c.randomChance(0.5)), // battered wool
+			LootEntry.of('minecraft:bamboo').when(c => c.randomChance(0.5)),
 			LootEntry.of('primitive_creatures:totem_0').when(c => c.randomChance(0.1)),
-			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)))
+			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)),
+			LootEntry.of('sns:quiver')) // battered wool
 	event.addEntityLootModifier('primitive_creatures:iloger_5')
 		.randomChance(CLOTHING_DROP_RATE)
 		.addWeightedLoot([
@@ -122,9 +122,9 @@ function registerPrimitiveCreaturesLoots(event) {
 		.addWeightedLoot([1,2], ['primitive_creatures:f_1']) // primitive explosives
 		.addWeightedLoot([1,3], ['minecraft:gunpowder'])
 		.addAlternativesLoot(
-			LootEntry.of('primitive_creatures:grh').when(c => c.randomChance(0.5)), // battered wool
 			LootEntry.of('primitive_creatures:totem_2').when(c => c.randomChance(0.1)),
-			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)))
+			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)),
+			LootEntry.of('primitive_creatures:grh')) // battered wool
 	event.addEntityLootModifier('primitive_creatures:iloger_6')
 		.randomChance(CLOTHING_DROP_RATE)
 		.addWeightedLoot([
@@ -138,9 +138,9 @@ function registerPrimitiveCreaturesLoots(event) {
 		.removeLoot(ItemFilter.ALWAYS_TRUE)
 		.addWeightedLoot([0,2], ['primitive_creatures:grh']) // battered wool
 		.addAlternativesLoot(
-			LootEntry.of('minecraft:lead').when(c => c.randomChance(0.4)),
 			LootEntry.of('primitive_creatures:totem_0').when(c => c.randomChance(0.1)),
-			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)))
+			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)),
+			LootEntry.of('minecraft:lead'))
 	event.addEntityLootModifier('primitive_creatures:wiloger')
 		.randomChance(CLOTHING_DROP_RATE)
 		.addWeightedLoot([
@@ -149,13 +149,13 @@ function registerPrimitiveCreaturesLoots(event) {
 			Item.of('tfc_textile:tiger_pants'),
 			Item.of('tfc_textile:tiger_boots')])
 
-	// huntsman - lion fur, holds flint club - stuns you in place when hit
+	// warrior - lion fur, holds flint club - stuns you in place when hit
 	event.addEntityLootModifier('primitive_creatures:piloger_9')
 		.removeLoot(ItemFilter.ALWAYS_TRUE)
 		.addAlternativesLoot(
 			LootEntry.of('primitive_creatures:yhgi').when(c => c.randomChance(0.2)), // flint club
 			LootEntry.of('tfc:small_raw_hide').when(c => c.randomChance(0.4)),
-			LootEntry.of('primitive_creatures:grh').when(c => c.randomChance(0.5))) // battered wool
+			LootEntry.of('primitive_creatures:grh')) // battered wool
 	event.addEntityLootModifier('primitive_creatures:piloger_9')
 		.randomChance(CLOTHING_DROP_RATE)
 		.addWeightedLoot([
@@ -169,10 +169,10 @@ function registerPrimitiveCreaturesLoots(event) {
 		.removeLoot(ItemFilter.ALWAYS_TRUE)
 		.addWeightedLoot([1,2], ['minecraft:emerald'])
 		.addAlternativesLoot(
-			LootEntry.of('primitive_creatures:grh').when(c => c.randomChance(0.8)),  // battered wool
 			LootEntry.of('gtceu:stone_knife').when(c => c.randomChance(0.3)),
 			LootEntry.of('primitive_creatures:totem_2').when(c => c.randomChance(0.1)),
-			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)))
+			LootEntry.of('primitive_creatures:totem_3').when(c => c.randomChance(0.1)),
+			LootEntry.of('primitive_creatures:grh'))  // battered wool)
 	event.addEntityLootModifier('primitive_creatures:viloger_10')
 		.randomChance(CLOTHING_DROP_RATE)
 		.addWeightedLoot([
@@ -181,9 +181,33 @@ function registerPrimitiveCreaturesLoots(event) {
 			Item.of('tfc_textile:raw_pants'),
 			Item.of('tfc_textile:raw_socks')])
 
+	// huntmaster - miniboss
+	event.addEntityLootModifier('primitive_creatures:nahida')
+		.removeLoot(ItemFilter.ALWAYS_TRUE)
+		.addWeightedLoot([
+			Item.of('gtceu:cobalt_brass_butchery_knife'),
+			Item.of('gtceu:cobalt_brass_axe'),
+			Item.of('gtceu:cobalt_brass_pickaxe'),
+			Item.of('gtceu:cobalt_brass_sword')])
+	event.addEntityLootModifier('primitive_creatures:nahida')
+		.addWeightedLoot([
+			Item.of('tfc:goat_horn'),
+			Item.of('sns:leather_sack'),
+			Item.of('sns:burlap_sack'),
+			Item.of('sns:seed_pouch'),
+			Item.of('sns:ore_sack')])
+	event.addEntityLootModifier('primitive_creatures:nahida')
+		.randomChance(CLOTHING_DROP_RATE)
+		.addWeightedLoot([
+			Item.of('tfc_textile:panther_hat'),
+			Item.of('tfc_textile:panther_shirt'),
+			Item.of('tfc_textile:panther_pants'),
+			Item.of('tfc_textile:panther_boots')])
+
 	// mistah beeeaaaasssstt
 	event.addEntityLootModifier('primitive_creatures:beast')
 		.removeLoot(ItemFilter.ALWAYS_TRUE)
 		.addWeightedLoot([4,8], ['minecraft:bone'])
+		.addWeightedLoot([2,3], ['tfc:large_raw_hide'])
 		// raw meat?
 }
