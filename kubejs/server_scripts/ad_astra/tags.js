734bb0bb9ecfcb51f179d2cc0174d351d4c8a447
Pyritie <pyritie@gmail.com>
2025-09-19 21:36:18 +0100

    feature rearranging

--- kubejs/server_scripts/ad_astra/tags.js
+++ kubejs/server_scripts/ad_astra/tags.js
@@ -492,16 +492,17 @@ const registerAdAstraPlacedFeatures = (event) => {
 	//event.add('tfg:mars_underground_decoration', 'tfg:mars/underground/cave_column')
 	//event.add('tfg:mars_underground_decoration', 'tfg:mars/underground/cave_spike')
 	//event.add('tfg:mars_underground_decoration', 'tfg:mars/underground/large_cave_spike')
+	event.add('tfg:mars_underground_decoration', 'tfg:mars/underground/ice_cave')
+
 	event.add('tfg:mars_underground_decoration', 'tfc:cave_spike')
 	event.add('tfg:mars_underground_decoration', 'tfc:cave_column')
 	event.add('tfg:mars_underground_decoration', 'tfc:large_cave_spike')
 
 	event.add('tfg:mars_underground_decoration', 'tfg:mars/underground/calcite')
 	event.add('tfg:mars_underground_decoration', 'tfg:mars/underground/mega_calcite')
-	event.add('tfg:mars_underground_decoration', 'tfc:icicle')
+	event.add('tfg:mars_underground_decoration', 'tfc:mars/underground/icicle')
 	event.add('tfg:mars_underground_decoration', 'tfg:mars/underground/loose_rocks')
 	event.add('tfg:mars_underground_decoration', 'tfg:mars/underground/ochrum_blob')
-	event.add('tfg:mars_underground_decoration', 'tfg:mars/underground/ice_cave')
 
 	event.add('tfg:mars_underground_decoration', "tfg:earth/geode/amethyst")
 	event.add('tfg:mars_underground_decoration', "tfg:earth/geode/barite")
