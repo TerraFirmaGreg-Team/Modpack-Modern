ca46b12ebe2d9ec8d0dec3cf7c2d5cd6850f1293
Pyritie <pyritie@gmail.com>
2025-09-30 18:15:02 +0100

    remove pcc recipes

--- kubejs/server_scripts/pccard/recipes.js
+++
@@ -1,8 +0,0 @@
-// priority: 0
-"use strict";
-
-function registerProgrammedCircuitCardRecipes(event) {
-
-	event.replaceInput({ id: 'pccard:item/card_programmed_circuit' }, 'gtceu:programmed_circuit', '#gtceu:circuits/lv')
-
-}
