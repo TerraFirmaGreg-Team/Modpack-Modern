184d1ace7e683b2fedabe6032f11603d1d83f7e8
TomPlop <tomdidome@gmail.com>
2025-10-04 12:51:38 -0400

    Remove Plutonium ore loot table and add a bad mixer recipe for bio diesel (#1955)

--- kubejs/server_scripts/tfg/recipes.miscellaneous.js
+++ kubejs/server_scripts/tfg/recipes.miscellaneous.js
@@ -571,10 +571,26 @@ function registerTFGMiscellaneousRecipes(event) {
 		.duration(20 * 10)
 		.EUt(GTValues.VHA[GTValues.ULV])
 
+	// So you can craft Biodiesel without Chemical Reactor
+	event.recipes.gtceu.mixer(`tfg:seed_oil_alcohol_biodiesel`)
+		.inputFluids("#tfc:alcohols 1000", Fluid.of('gtceu:seed_oil', 1000))
+		.itemInputs('#forge:tiny_dusts/sodium_hydroxide')
+		.outputFluids(Fluid.of('gtceu:bio_diesel', 500))
+		.duration(20 * 10)
+		.EUt(GTValues.VHA[GTValues.ULV])
+
 	event.recipes.gtceu.chemical_reactor(`olive_oil_alcohol_biodiesel`)
 		.inputFluids("#tfc:alcohols 1000", Fluid.of('tfc:olive_oil', 4000))
 		.itemInputs('#forge:tiny_dusts/sodium_hydroxide')
 		.outputFluids(Fluid.of('gtceu:bio_diesel', 6000))
+		.duration(20 * 10)
+		.EUt(GTValues.VHA[GTValues.ULV])
+
+	// So you can craft Biodiesel without Chemical Reactor
+	event.recipes.gtceu.mixer(`tfg:olive_oil_alcohol_biodiesel`)
+		.inputFluids("#tfc:alcohols 1000", Fluid.of('tfc:olive_oil', 1000))
+		.itemInputs('#forge:tiny_dusts/sodium_hydroxide')
+		.outputFluids(Fluid.of('gtceu:bio_diesel', 500))
 		.duration(20 * 10)
 		.EUt(GTValues.VHA[GTValues.ULV])
 
@@ -585,10 +601,26 @@ function registerTFGMiscellaneousRecipes(event) {
 		.duration(20 * 10)
 		.EUt(GTValues.VHA[GTValues.ULV])
 
+	// So you can craft Biodiesel without Chemical Reactor
+	event.recipes.gtceu.mixer(`tfg:soybean_oil_alcohol_biodiesel`)
+		.inputFluids("#tfc:alcohols 1000", Fluid.of('firmalife:soybean_oil', 1000))
+		.itemInputs('#forge:tiny_dusts/sodium_hydroxide')
+		.outputFluids(Fluid.of('gtceu:bio_diesel', 500))
+		.duration(20 * 10)
+		.EUt(GTValues.VHA[GTValues.ULV])
+
 	event.recipes.gtceu.chemical_reactor(`fish_oil_alcohol_biodiesel`)
 		.inputFluids("#tfc:alcohols 1000", Fluid.of('gtceu:fish_oil', 6000))
 		.itemInputs('#forge:tiny_dusts/sodium_hydroxide')
 		.outputFluids(Fluid.of('gtceu:bio_diesel', 6000))
+		.duration(20 * 10)
+		.EUt(GTValues.VHA[GTValues.ULV])
+
+	// So you can craft Biodiesel without Chemical Reactor
+	event.recipes.gtceu.mixer(`tfg:fish_oil_alcohol_biodiesel`)
+		.inputFluids("#tfc:alcohols 1000", Fluid.of('gtceu:fish_oil', 1000))
+		.itemInputs('#forge:tiny_dusts/sodium_hydroxide')
+		.outputFluids(Fluid.of('gtceu:bio_diesel', 500))
 		.duration(20 * 10)
 		.EUt(GTValues.VHA[GTValues.ULV])
 
