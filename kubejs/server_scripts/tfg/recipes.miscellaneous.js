4993cff9dc4c348970bf07ae469274495ee32ce9
dimethylene <127608685+dimethylene@users.noreply.github.com>
2025-10-11 19:23:46 -0400

    Changed to using gregtech materials instead

--- kubejs/server_scripts/tfg/recipes.miscellaneous.js
+++ kubejs/server_scripts/tfg/recipes.miscellaneous.js
@@ -1459,13 +1459,4 @@ function registerTFGMiscellaneousRecipes(event) {
 		.duration(300)
 		.EUt(GTValues.VA[GTValues.EV])
 	
-	Ingredient.of("#forge:impure_dusts").getItemIds().forEach(itemid => {
-		event.recipes.tfc.barrel_instant()
-			.inputItem(`1x ${itemid}`)
-			.inputFluid(Fluid.of("minecraft:water", 100))
-			.outputItem(`1x ${itemid.replace("impure_", "")}`)
-			.id(`tfg:tfc/barrel/${itemid.replace("gtceu:", "")}`)
-			
-	})
-	
 }
