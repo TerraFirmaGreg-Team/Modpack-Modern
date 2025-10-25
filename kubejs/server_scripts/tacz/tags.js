253135b21bcbf653d89baf529a0e0fccf2ca9be1
GameStar <56610486+BlueBoat29@users.noreply.github.com>
2025-10-25 14:41:45 -0500

    Add tacz ammo boxes (#2104)
    * stuff
    
    * Update CHANGELOG.md
    
    Signed-off-by: GameStar <56610486+BlueBoat29@users.noreply.github.com>
    
    * Update CHANGELOG.md
    
    Signed-off-by: GameStar <56610486+BlueBoat29@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: GameStar <56610486+BlueBoat29@users.noreply.github.com>

--- kubejs/server_scripts/tacz/tags.js
+++ kubejs/server_scripts/tacz/tags.js
@@ -2,13 +2,18 @@
 
 const registerTACZItemTags = (event) => {
 	
-	event.add('c:hidden_from_recipe_viewers', 'tacz:ammo_box')
+	//event.add('c:hidden_from_recipe_viewers', 'tacz:ammo_box')
 	event.add('c:hidden_from_recipe_viewers', 'tacz:gun_smith_table')
 	event.add('c:hidden_from_recipe_viewers', 'tacz:workbench_a')
 	event.add('c:hidden_from_recipe_viewers', 'tacz:workbench_b')
 	event.add('c:hidden_from_recipe_viewers', 'tacz:workbench_c')
 
+	event.add('tfc:usable_on_tool_rack', 'tacz:ammo_box')
+	event.add('tfc:usable_on_tool_rack', 'tacz:modern_kinetic_gun')
+	
+}
 
+const registerTACZBlockTags = (event) => {
 	
-	
+	event.add('tacz:interact_key/whitelist', '#tfc:tool_racks')
 }
