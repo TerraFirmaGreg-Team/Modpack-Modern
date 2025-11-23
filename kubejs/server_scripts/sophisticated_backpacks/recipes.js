9b5a4d01f8a13be7c1d2dc6abe0372eb832898b5
Morgan Arnold <62062835+CrashAndSideburns@users.noreply.github.com>
2025-11-23 22:43:40 +0100

    Update gating of Sophisticated Backpacks upgrades
    This commit makes the pickup upgrade and the advanced pickup upgrade from Sophisticated Backpacks accessible at LV. It also makes the magnet upgrade and the advanced magnet upgrade harder to obtain, requiring MV.
    
    Signed-off-by: Morgan Arnold <62062835+CrashAndSideburns@users.noreply.github.com>

--- kubejs/server_scripts/sophisticated_backpacks/recipes.js
+++ kubejs/server_scripts/sophisticated_backpacks/recipes.js
@@ -179,18 +179,18 @@ const registerSophisticatedBackpacksRecipes = (event) => {
         'CDE',
         'FGF'
     ], {
-        A: 'gtceu:hv_electric_motor',
-        B: 'gtceu:mercury_barium_calcium_cuprate_single_wire',
-        C: 'gtceu:hv_conveyor_module',
+        A: 'gtceu:lv_electric_motor',
+        B: 'gtceu:item_filter',
+        C: 'gtceu:lv_conveyor_module',
         D: 'sophisticatedbackpacks:upgrade_base',
-        E: 'gtceu:hv_robot_arm',
-        F: '#gtceu:circuits/hv',
-        G: '#gtceu:batteries/hv'
+        E: 'gtceu:lv_robot_arm',
+        F: '#gtceu:circuits/lv',
+        G: '#gtceu:batteries/lv'
     }).id('tfg:sophisticated_backpacks/shaped/pickup_upgrade')
 
     event.shapeless('sophisticatedbackpacks:advanced_pickup_upgrade', [
         'sophisticatedbackpacks:pickup_upgrade',
-        '#gtceu:circuits/ev'
+        'gtceu:smart_item_filter'
     ]).id('tfg:sophisticated_backpacks/shapeless/advanced_pickup_upgrade')
 
     event.shaped('sophisticatedbackpacks:filter_upgrade', [
@@ -213,23 +213,37 @@ const registerSophisticatedBackpacksRecipes = (event) => {
         '#gtceu:circuits/mv'
     ]).id('tfg:sophisticated_backpacks/shapeless/advanced_filter_upgrade')
 
-    event.shaped('sophisticatedbackpacks:magnet_upgrade', [
+	event.shaped('sophisticatedbackpacks:magnet_upgrade', [
         'ABA',
-        'CDC',
-        'FEG'
+        'CDE',
+        'FGF'
     ], {
-        A: 'gtceu:lv_robot_arm',
-        B: 'gtceu:item_filter',
-        C: '#gtceu:circuits/lv',
-        D: 'sophisticatedbackpacks:upgrade_base',
-        E: 'gtceu:lv_item_magnet',
-        F: '#forge:tools/wrenches',
-        G: '#forge:tools/screwdrivers'
+        A: 'gtceu:mv_electric_motor',
+        B: 'gtceu:mv_item_magnet',
+        C: 'gtceu:mv_conveyor_module',
+        D: 'sophisticatedbackpacks:pickup_upgrade',
+        E: 'gtceu:mv_robot_arm',
+        F: '#gtceu:circuits/mv',
+        G: '#gtceu:batteries/mv'
     }).id('tfg:sophisticated_backpacks/shaped/magnet_upgrade')
+
+	event.shaped('sophisticatedbackpacks:advanced_magnet_upgrade', [
+        'ABA',
+        'CDE',
+        'FGF'
+    ], {
+        A: 'gtceu:mv_electric_motor',
+        B: 'gtceu:mv_item_magnet',
+        C: 'gtceu:mv_conveyor_module',
+        D: 'sophisticatedbackpacks:advanced_pickup_upgrade',
+        E: 'gtceu:mv_robot_arm',
+        F: '#gtceu:circuits/mv',
+        G: '#gtceu:batteries/mv'
+    }).id('tfg:sophisticated_backpacks/shaped/advanced_magnet_upgrade')
 
     event.shapeless('sophisticatedbackpacks:advanced_magnet_upgrade', [
         'sophisticatedbackpacks:magnet_upgrade',
-        'gtceu:hv_item_magnet'
+        'gtceu:smart_item_filter'
     ]).id('tfg:sophisticated_backpacks/shapeless/advanced_magnet_upgrade')
 
     event.shaped('sophisticatedbackpacks:void_upgrade', [
