0d75405aee6a00d522e1e175b4da3ab51b90265c
Redeix <brayden.j.m.ford@gmail.com>
2025-12-09 00:11:17 -0600

    why doesnt rhino just have classes? (#2355)

--- kubejs/startup_scripts/ad_astra/constants.js
+++ kubejs/startup_scripts/ad_astra/constants.js
@@ -1,4 +1,4 @@
-// priority: 0
+// priority: -100
 "use strict";
 
 global.AD_ASTRA_DISABLED_ITEMS = 
@@ -110,7 +110,37 @@ global.BREATHABLE_COMPRESSED_AIRS =
 ];
 
 global.AD_ASTRA_WOOD = [
-	{name: 'aeronos', logs: '#ad_astra:aeronos_caps', log: 'ad_astra:aeronos_stem', stripped_log: null, lumber: 'tfg:wood/lumber/aeronos', plank: 'ad_astra:aeronos_planks', stair: 'ad_astra:aeronos_stairs', slab: 'ad_astra:aeronos_slab', fence: 'ad_astra:aeronos_fence', fence_gate: 'ad_astra:aeronos_fence_gate', door: 'ad_astra:aeronos_door', trapdoor: 'ad_astra:aeronos_trapdoor', button: null, pressure_plate: null, support: 'tfg:aeronos_support', isHardwood: false},
-	{name: 'strophar', logs: '#ad_astra:strophar_caps', log: 'ad_astra:strophar_stem', stripped_log: null, lumber: 'tfg:wood/lumber/strophar', plank: 'ad_astra:strophar_planks', stair: 'ad_astra:strophar_stairs', slab: 'ad_astra:strophar_slab', fence:'ad_astra:strophar_fence', fence_gate: 'ad_astra:strophar_fence_gate', door:'ad_astra:strophar_door', trapdoor: 'ad_astra:strophar_trapdoor', button: null, pressure_plate: null, support: 'tfg:strophar_support', isHardwood: false},
-	{name: 'glacian', logs: '#ad_astra:glacian_logs', log: 'ad_astra:glacian_log', stripped_log: 'ad_astra:stripped_glacian_log', lumber: 'tfg:wood/lumber/glacian', plank: 'ad_astra:glacian_planks', stair: 'ad_astra:glacian_stairs', slab: 'ad_astra:glacian_slab', fence: 'ad_astra:glacian_fence', fence_gate: 'ad_astra:glacian_fence_gate', door: 'ad_astra:glacian_door', trapdoor: 'ad_astra:glacian_trapdoor', button: 'ad_astra:glacian_button', pressure_plate: 'ad_astra:glacian_pressure_plate', support: 'tfg:glacian_support', isHardwood: true},
+	{
+		name: 'aeronos',
+		logs: '#ad_astra:aeronos_caps', log: 'ad_astra:aeronos_stem', stripped_log: null,
+		lumber: 'tfg:wood/lumber/aeronos', plank: 'ad_astra:aeronos_planks',
+		stair: 'ad_astra:aeronos_stairs', slab: 'ad_astra:aeronos_slab',
+		fence: 'ad_astra:aeronos_fence', fence_gate: 'ad_astra:aeronos_fence_gate',
+		door: 'ad_astra:aeronos_door', trapdoor: 'ad_astra:aeronos_trapdoor',
+		button: null, pressure_plate: null,
+		support: 'tfg:aeronos_support',
+		isHardwood: false
+	},
+	{
+		name: 'strophar',
+		logs: '#ad_astra:strophar_caps', log: 'ad_astra:strophar_stem', stripped_log: null,
+		lumber: 'tfg:wood/lumber/strophar', plank: 'ad_astra:strophar_planks',
+		stair: 'ad_astra:strophar_stairs', slab: 'ad_astra:strophar_slab',
+		fence:'ad_astra:strophar_fence', fence_gate: 'ad_astra:strophar_fence_gate',
+		door:'ad_astra:strophar_door', trapdoor: 'ad_astra:strophar_trapdoor',
+		button: null, pressure_plate: null,
+		support: 'tfg:strophar_support',
+		isHardwood: false
+	},
+	{
+		name: 'glacian',
+		logs: '#ad_astra:glacian_logs', log: 'ad_astra:glacian_log', stripped_log: 'ad_astra:stripped_glacian_log',
+		lumber: 'tfg:wood/lumber/glacian', plank: 'ad_astra:glacian_planks',
+		stair: 'ad_astra:glacian_stairs', slab: 'ad_astra:glacian_slab',
+		fence: 'ad_astra:glacian_fence', fence_gate: 'ad_astra:glacian_fence_gate',
+		door: 'ad_astra:glacian_door', trapdoor: 'ad_astra:glacian_trapdoor',
+		button: 'ad_astra:glacian_button', pressure_plate: 'ad_astra:glacian_pressure_plate',
+		support: 'tfg:glacian_support',
+		isHardwood: true
+	}
 ]
--- kubejs/startup_scripts/horornot/constants.js
+++ kubejs/startup_scripts/horornot/constants.js
@@ -1,4 +1,4 @@
-// priority: 0
+// priority: -100
 "use strict";
 
 global.HOT_OR_NOT_DISABLED_ITEMS = [
