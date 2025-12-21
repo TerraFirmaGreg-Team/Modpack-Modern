7777bb10ead93b1c85f0d9dfcf2fb544df1035e1
bigtho80 <97521928+bigtho80@users.noreply.github.com>
2025-12-21 16:59:42 +0100

    rename cancelalcoholbucket.js into nonplaceableliquid.js and edit to add tfc:nonplaceable tag
    Signed-off-by: bigtho80 <97521928+bigtho80@users.noreply.github.com>

--- kubejs/server_scripts/tfc/cancelalcoholbucket.js
+++
@@ -1,7 +0,0 @@
-ItemEvents.rightClicked(event => {
-    const id = event.item.id
-
-    if (id.startsWith('tfc:bucket/') || id.startsWith('tfcagedalcohol:bucket/')) {
-        event.cancel()
-    }
-})
