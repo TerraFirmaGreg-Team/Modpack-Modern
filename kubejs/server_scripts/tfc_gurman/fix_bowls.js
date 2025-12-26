b5c6c9f742660438a74c9ab37a7f4afd20f1a304
BlackLightDragon <108180227+BlackLightDragon@users.noreply.github.com>
2025-12-26 01:04:54 +0100

    make it actually work: fix_bowls.js
    Signed-off-by: BlackLightDragon <108180227+BlackLightDragon@users.noreply.github.com>

--- kubejs/server_scripts/tfc_gurman/fix_bowls.js
+++ kubejs/server_scripts/tfc_gurman/fix_bowls.js
@@ -1,3 +1,4 @@
+//list of foods that should return a bowl
 let bowls = [
     "porridge",
     "porridge_with_honey",
@@ -33,13 +34,30 @@ let bowls = [
     "borscht"
 ]
 
-for (let i=0; i<bowls.length; i++){
-    bowls[i] = "tfc_gurman:"+bowls[i]
+function return_bowl(event){
+    const { player, server } = event
+    
+    // Create a unique key for this player and item
+    let lastEatenKey = `bowl_fix_timer_${player.uuid}`
+    let currentTime = server.tickCount
+
+    // Check if the player already ate one of the effected items so that duplication doesn't occure
+    // (foodEaten event is triggered multible times because of nutrition or smth)
+    // (cooldown should be able to set to 1 as these events should trigger in the same tick as it was eaten maybe)
+    if (!player.persistentData.contains(lastEatenKey) || 
+        (currentTime - player.persistentData.getInt(lastEatenKey)) > 10) {
+        
+        player.give('tfc:ceramic/bowl')
+        
+        // Update the timestamp
+        player.persistentData.putInt(lastEatenKey, currentTime)
+    }
 }
 
-ItemEvents.foodEaten( event => {
-    
-    if(bowls.includes("tfc_gurman:borscht")) {
-        event.player.give(Item.of("tfc:ceramic/bowl",1))
-    }
-})
+// Create Events for every entry in bowls
+
+for (let i = 0; i < bowls.length; i++){
+    ItemEvents.foodEaten("tfc_gurman:"+bowls[i], event => {
+        return_bowl(event)
+    })
+}
