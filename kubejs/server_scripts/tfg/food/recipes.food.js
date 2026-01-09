7b51fbdc34e16285053c40b9aee92dd20f3439a6
Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>
2026-01-08 21:17:18 -0300

    Add vinegar as option for curdled milk crafting
    This adds an earlier way to make cheese without relying on acetic acid. As vinegar is partially composed of acetic acid, and the recipe for homemade cheese using vinegar is realistic, that would be an useful and accurate recipe.
    
    Signed-off-by: Jean Roberto de Souza <81846511+JeanRdSz@users.noreply.github.com>

--- kubejs/server_scripts/tfg/food/recipes.food.js
+++ kubejs/server_scripts/tfg/food/recipes.food.js
@@ -276,6 +276,14 @@ function registerTFGFoodRecipes(event) {
 			.duration(300)
 			.EUt(GTValues.VA[GTValues.LV])
 
+		event.recipes.gtceu.mixer(`lactose_milk_vinegar_${item.id}`)
+			.circuit(1)
+			.inputFluids(Fluid.of(item.milk, 1000), Fluid.of('tfc:vinegar', 50))
+			.itemOutputs('1x gtceu:lactose_dust')
+			.outputFluids(Fluid.of(item.curdled_fluid, 1000))
+			.duration(300)
+			.EUt(GTValues.VA[GTValues.LV])
+
 		global.processorRecipe(event, `${item.id}_curd`, 1200, 16, {
 			itemOutputs: [item.curd],
 			fluidInputs: [Fluid.of(item.curdled_fluid, 1000)],
