9e26d26f10e63d0de4f1993ba206a6f4cb138d89
Redeix <brayden.j.m.ford@gmail.com>
2026-01-25 00:04:24 -0600

    recipes

--- kubejs/server_scripts/tfg/primitive/recipes.artisan_table.js
+++ kubejs/server_scripts/tfg/primitive/recipes.artisan_table.js
@@ -3,299 +3,495 @@
 
 function registerTFGArtisanTableRecipes(event) {
 
+	// Removals (`type` didnt want to work here).
+	event.remove({ id: /gtceu:shaped\/shape_.*/, input: 'gtceu:empty_mold'});
+	event.remove({ id: /gtceu:shaped\/shape_.*/, input: /gtceu:.*_extruder_mold/});
+	event.remove({ id: 'gtceu:shaped/basic_circuit_board'});
+	event.remove({ id: 'gtceu:shaped/good_circuit_board'});
+
 	//#region Molds
-	event.remove({input: 'gtceu:empty_mold', type: 'minecraft:crafting'})
-	
-	event.custom({
-		"result": {"item": "tfg:lamp_casting_mold"},
-		"pattern": [
+
+	/** 
+	 * Mold pattern definitions.
+	 * @param {string} name - Base id of the mold item.
+	 * @param {boolean} genCast - Whether to generate a casting mold recipe.
+	 * @param {boolean} genExtrude - Whether to generate an extruder mold recipe.
+	 * @param {string[]} pattern - 6x6 pattern for the mold. 'X' for solid, ' ' for empty.
+	 * @type {{name: string, genCast: boolean, genExtrude: boolean, pattern: string[]}[]} moldPatterns 
+	 */
+	const moldPatterns = [
+		{ name: 'tfg:lamp', genCast: true, genExtrude: false, pattern: [
 			"XX  XX",
 			"X    X",
 			"X XX X",
 			"X XX X",
 			"X XX X",
 			"X    X"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"
-	}).id(`tfg:artisan_table/lamp_casting_mold`);
-
-	event.custom({
-		"result": {"item": "tfg:trapdoor_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'tfg:trapdoor', genCast: true, genExtrude: false, pattern: [
 			"      ",
 			" XXXX ",
 			"      ",
 			"      ",
 			" XXXX ",
 			"      "
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/trapdoor_casting_mold`);
-
-	event.custom({
-		"result": {"item": "tfg:chain_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'tfg:chain', genCast: true, genExtrude: false, pattern: [
 			"X    X",
 			"X XX X",
 			"X    X",
 			"XX  XX",
 			"X    X",
 			"X XX X"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/chain_casting_mold`);
-
-	event.custom({
-		"result": {"item": "tfg:bell_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'tfg:bell', genCast: true, genExtrude: false, pattern: [
 			"XX  XX",
 			"X    X",
 			"X    X",
 			"X    X",
 			"      ",
 			"      "
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/bell_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:plate_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:plate', genCast: true, genExtrude: true, pattern: [
 			"XXXXXX",
 			"X    X",
 			"X    X",
 			"X    X",
 			"X    X",
 			"XXXXXX"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/plate_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:gear_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:gear', genCast: true, genExtrude: true, pattern: [
 			"XX  XX",
 			"X    X",
 			"  XX  ",
 			"  XX  ",
 			"X    X",
 			"XX  XX"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/gear_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:bottle_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:bottle', genCast: true, genExtrude: true, pattern: [
 			"X    X",
 			"XX  XX",
 			"XX  XX",
 			"X    X",
 			"X    X",
 			"X    X"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/bottle_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:ingot_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:ingot', genCast: true, genExtrude: true, pattern: [
 			"XXXXXX",
 			"XX  XX",
 			"XX  XX",
 			"XX  XX",
 			"XX  XX",
 			"XXXXXX"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/ingot_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:ball_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:ball', genCast: true, genExtrude: false, pattern: [
 			"XXXXXX",
 			"XX  XX",
 			"X    X",
 			"X    X",
 			"XX  XX",
 			"XXXXXX"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/ball_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:cylinder_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:cylinder', genCast: true, genExtrude: false, pattern: [
 			"XX  XX",
 			"X    X",
 			"      ",
 			"      ",
 			"X    X",
 			"XX  XX"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/cylinder_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:block_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:block', genCast: true, genExtrude: true, pattern: [
 			"      ",
 			"      ",
 			"      ",
 			"      ",
 			"      ",
 			"      "
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/block_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:nugget_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:nugget', genCast: true, genExtrude: false, pattern: [
 			"XXXXXX",
 			"XXXXXX",
 			"XX XXX",
 			"XX XXX",
 			"XXXXXX",
 			"XXXXXX"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/nugget_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:anvil_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:anvil', genCast: true, genExtrude: false, pattern: [
 			"      ",
 			"      ",
 			"X    X",
 			"XX  XX",
 			"X    X",
 			"XXXXXX"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/anvil_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:name_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:name', genCast: true, genExtrude: false, pattern: [
 			"XXXXXX",
 			"X X X ",
 			" X X X",
 			"X X X ",
 			"XXXXXX",
 			"XXXXXX"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/name_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:small_gear_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:small_gear', genCast: true, genExtrude: true, pattern: [
 			"XXX XX",
 			"XX   X",
 			"X  X  ",
 			"XX   X",
 			"XXX XX",
 			"XXXXXX"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/small_gear_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:rotor_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:rotor', genCast: true, genExtrude: true, pattern: [
 			" XXXX ",
 			"X XX X",
 			"XX  XX",
 			"XX  XX",
 			"X XX X",
 			" XXXX "
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/rotor_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:pill_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:pill', genCast: true, genExtrude: false, pattern: [
 			"XXXXXX",
 			"XXXXXX",
 			"XX  XX",
 			"X   XX",
 			"X  XXX",
 			"XXXXXX"
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/pill_casting_mold`);
-
-	event.custom({
-		"result": {"item": "gtceu:huge_pipe_casting_mold"},
-		"pattern": [
+		]},
+		{ name: 'gtceu:huge_pipe', genCast: true, genExtrude: true, pattern: [
 			"      ",
 			" XXXX ",
 			" XXXX ",
 			" XXXX ",
 			" XXXX ",
 			"      "
-		],
-		"artisanType": "tfg:casting_mold",
-		"type": "tfg:artisan"	
-	}).id(`tfg:artisan_table/huge_pipe_casting_mold`);
-	
-	//#endregion
+		]},
+		
+		{ name: 'gtceu:rod', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XXXXXX",
+			"XX  XX",
+			"XX  XX",
+			"XXXXXX",
+			"XXXXXX"
+		]},
+		{ name: 'gtceu:bolt', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XXXXXX",
+			"XX   X",
+			"XXX XX",
+			"XXX XX",
+			"XXXXXX"
+		]},
+		{ name: 'gtceu:ring', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XX  XX",
+			"X XX X",
+			"X XX X",
+			"XX  XX",
+			"XXXXXX"
+		]},
+		{ name: 'gtceu:cell', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XX   X",
+			"X XX X",
+			"X XX X",
+			"X   XX",
+			"XXXXXX"
+		]},
+		{ name: 'gtceu:wire', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XX XXX",
+			"XXXXXX",
+			"XXXXXX",
+			"XXX XX",
+			"XXXXXX"
+		]},
+		{ name: 'gtceu:foil', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XXXXXX",
+			"XXXXXX",
+			"      ",
+			"XXXXXX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:small_casing', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XXXXXX",
+			"X X X ",
+			"XXXXXX",
+			"X X X ",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:shell_casing', genCast: false, genExtrude: true, pattern: [
+			"  XXXX",
+			"  XXXX",
+			"XXXXXX",
+			"XXXXXX",
+			"XXXX  ",
+			"XXXX  "
+		]},
+		{ name: 'tfg:large_casing', genCast: false, genExtrude: true, pattern: [
+			"XX  XX",
+			"XX  XX",
+			"  XXXX",
+			"  XXXX",
+			"XXXX  ",
+			"XXXX  "
+		]},
 
-	//#region Pipes
+		//Tools
+		{ name: 'tfg:mining_hammer_head', genCast: false, genExtrude: true, pattern: [
+			" XXXXX",
+			"     X",
+			"      ",
+			"     X",
+			" XXXXX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:sword_head', genCast: false, genExtrude: true, pattern: [
+			"  XXXX",
+			"   XXX",
+			"X   XX",
+			"XX   X",
+			"XXX  X",
+			"XXXXX "
+		]},
+		{ name: 'tfg:pickaxe_head', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XXXXXX",
+			"X    X",
+			" XXXX ",
+			"XXXXXX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:shovel_head', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"X    X",
+			"X    X",
+			"X    X",
+			"XX  XX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:axe_head', genCast: false, genExtrude: true, pattern: [
+			"X XXXX",
+			"     X",
+			"      ",
+			"     X",
+			"X XXXX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:hoe_head', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XXXXXX",
+			"      ",
+			"  XXXX",
+			"XXXXXX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:scythe_head', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"X     ",
+			"    XX",
+			"  XXXX",
+			"XXXXXX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:file_head', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XX XXX",
+			"XX XXX",
+			"XX XXX",
+			"XX XXX",
+			"XX XXX"
+		]},
+		{ name: 'tfg:hammer_head', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"      ",
+			"      ",
+			"XX  XX",
+			"XXXXXX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:saw_head', genCast: false, genExtrude: true, pattern: [
+			"  XXXX",
+			"   XXX",
+			"X   XX",
+			"X    X",
+			"XX    ",
+			"XXXX  "
+		]},
+		{ name: 'tfg:knife_head', genCast: false, genExtrude: true, pattern: [
+			"XXX XX",
+			"XX  XX",
+			"XX  XX",
+			"XX  XX",
+			"XX  XX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:butchery_knife_head', genCast: false, genExtrude: true, pattern: [
+			"X XXXX",
+			"X XXXX",
+			"X   XX",
+			"X    X",
+			"X  X X",
+			"X    X"
+		]},
+		{ name: 'tfg:spade_head', genCast: false, genExtrude: true, pattern: [
+			"X    X",
+			"X    X",
+			"X    X",
+			"X    X",
+			"XX  XX",
+			"XX  XX"
+		]},
+		{ name: 'tfg:propick_head', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"     X",
+			" XXXX ",
+			" XXXXX",
+			"XXXXXX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:javelin_head', genCast: false, genExtrude: true, pattern: [
+			"   XXX",
+			"    XX",
+			"     X",
+			"X     ",
+			"XX   X",
+			"XXX XX"
+		]},
+		{ name: 'tfg:chisel_head', genCast: false, genExtrude: true, pattern: [
+			"XXX XX",
+			"XXX XX",
+			"XXX XX",
+			"XXX XX",
+			"XXX XX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:mace_head', genCast: false, genExtrude: true, pattern: [
+			"XX  XX",
+			"X    X",
+			"X    X",
+			"X    X",
+			"X    X",
+			"XX  XX"
+		]},
+		{ name: 'tfg:mattock_head', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"     X",
+			"   XX ",
+			"XXXXXX",
+			"XXXXXX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:fish_hook', genCast: false, genExtrude: true, pattern: [
+			"XXX XX",
+			"XXXX X",
+			"X XX X",
+			"X XX X",
+			"XX  XX",
+			"XXXXXX"
+		]},
+		{ name: 'tfg:whisk', genCast: false, genExtrude: true, pattern: [
+			" X  X ",
+			"XX  XX",
+			"  XX  ",
+			"  XX  ",
+			"XX  XX",
+			" X  X "
+		]},
+		{ name: 'tfg:screwdriver_tip', genCast: false, genExtrude: true, pattern: [
+			"XX   X",
+			"XXX XX",
+			"XX   X",
+			"XXX XX",
+			"XXX XX",
+			"XXX XX"
+		]},
+		{ name: 'tfg:wrench_tip', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XX XXX",
+			"XX XXX",
+			"XX XXX",
+			"XX XXX",
+			"X   XX"
+		]},
+		{ name: 'tfg:wire_cutter_head', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XX   X",
+			"X X  X",
+			"X  XXX",
+			"X  XXX",
+			"XXXXXX"
+		]}
 
-	/** @type {string[]} pipe_sizes */
-	const pipe_sizes = [
-		'huge',
-		'large',
-		'normal',
-		'small',
-		'tiny'
+		/*
+		,
+		{ name: 'tfg:', genCast: false, genExtrude: true, pattern: [
+			"XXXXXX",
+			"XXXXXX",
+			"XXXXXX",
+			"XXXXXX",
+			"XXXXXX",
+			"XXXXXX"
+		]}
+		*/
 	];
-	
-	let prev_size;
-	pipe_sizes.forEach((size) => {
-		if (size !== 'huge'){
-			event.shaped(`gtceu:${size}_pipe_casting_mold`, [
-				'ABC'
-			], {
-				A: '#forge:tools/hammers',
-				B: `gtceu:${prev_size}_pipe_casting_mold`,
-				C: '#forge:tools/mallets'
-			}).id(`tfg:shaped/${prev_size}_${size}_casting_mold`)
-		}
-		prev_size = size;
-	});
-	
-	pipe_sizes.reverse().forEach((size) => {
-		if (size !== 'tiny'){
-			event.shaped(`gtceu:${size}_pipe_casting_mold`, [
-				'ABC'
-			], {
-				A: '#forge:tools/mallets',
-				B: `gtceu:${prev_size}_pipe_casting_mold`,
-				C: '#forge:tools/hammers'
-			}).id(`tfg:shaped/${prev_size}_${size}_casting_mold`)
-		}
-		prev_size = size;
+
+	/** Checks for duplicate patterns for the same mold type. */
+	function patternsEqual(p1, p2) {
+		for (let i = 0; i < p1.length; i++) {
+			if (p1[i] !== p2[i]) return false;
+		};
+		return true;
+	};
+	/** Checks for duplicate mirrored patterns for the same mold type. */
+	function patternsMirrorEqual(p1, p2) {
+		for (let i = 0; i < p1.length; i++) {
+			if (p1[i] !== p2[i].split('').reverse().join('')) return false;
+		};
+		return true;
+	};
+	// Error logging for duplicate patterns.
+	let patternsChecked = [];
+	for (let i = 0; i < moldPatterns.length; i++) {
+		for (let j = i + 1; j < moldPatterns.length; j++) {
+
+			let a = moldPatterns[i];
+			let b = moldPatterns[j];
+			let shareCast = a.genCast && b.genCast;
+			let shareExtrude = a.genExtrude && b.genExtrude;
+
+			if (shareCast || shareExtrude) {
+				if (patternsEqual(a.pattern, b.pattern) || patternsMirrorEqual(a.pattern, b.pattern)) {
+					patternsChecked.push({a: a.name, b: b.name});
+				};
+			};
+		};
+	};
+	if (patternsChecked.length !== 0) {
+		throw new Error(
+			`Duplicate artisan table mold patterns detected between: ${patternsChecked.map(pair => `\n${pair.a} and ${pair.b}`).join(', ')}`
+		);
+	};
+
+	// Create Artisan Table recipes for molds.
+	moldPatterns.forEach((mold) => {	
+		if (mold.genCast){
+			event.custom({
+				"result": {"item": `${mold.name}_casting_mold`},
+				"pattern": mold.pattern,
+				"artisanType": "tfg:casting_mold",
+				"type": "tfg:artisan"
+			}).id(`tfg:artisan_table/${linuxUnfucker(mold.name)}_casting_mold`);
+		};	
+		if (mold.genExtrude){
+			event.custom({
+				"result": {"item": `${mold.name}_extruder_mold`},
+				"pattern": mold.pattern,
+				"artisanType": "tfg:extruder_mold",
+				"type": "tfg:artisan"
+			}).id(`tfg:artisan_table/${linuxUnfucker(mold.name)}_extruder_mold`);
+		};
 	});
 	
 	//#endregion
