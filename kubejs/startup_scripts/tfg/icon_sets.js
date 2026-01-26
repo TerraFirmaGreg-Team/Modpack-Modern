ac40131c308351cdd707a709501bc4db9868f342
RubenVerg <RubenVerg@outlook.com>
2026-01-26 16:17:25 +0100

    Create a new material icon set `noodle` and give it to vanadium

--- kubejs/startup_scripts/tfg/icon_sets.js
+++ kubejs/startup_scripts/tfg/icon_sets.js
@@ -37,6 +37,8 @@ function registerTFGIconSets(event) {
 		
 	event.create('nether_quartz').parent('quartz')
 
+    event.create('noodle').parent('metallic')
+
     // Credits to Flare from the Cosmic Frontiers team
     event.create('chonky').parent('shiny')
 }
@@ -151,6 +153,8 @@ function modifyTFGIconSets(event) {
     GTMaterials.RutheniumTriniumAmericiumNeutronate.setMaterialIconSet(GTMaterialIconSet.getByName('chonky'))
 
     GTMaterials.Zirconium.setMaterialIconSet(GTMaterialIconSet.getByName('chonky'))
+
+    GTMaterials.Vanadium.setMaterialIconSet(GTMaterialIconSet.getByName('noodle'))
 
     const $GreateMaterials = Java.loadClass("electrolyte.greate.registry.GreateMaterials")
 	$GreateMaterials.RoseQuartz.setMaterialIconSet(GTMaterialIconSet.getByName('nether_quartz'))
