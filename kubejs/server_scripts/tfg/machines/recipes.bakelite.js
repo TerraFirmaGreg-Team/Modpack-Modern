9b31d2439aab71519ac8a495203834cc734a24af
Pyritie <pyritie@gmail.com>
2026-01-27 01:12:22 +0000

    move PE to MV, langs, tweak some quests and add another MV phenolic board recipe

--- kubejs/server_scripts/tfg/machines/recipes.bakelite.js
+++ kubejs/server_scripts/tfg/machines/recipes.bakelite.js
@@ -2,8 +2,22 @@
 
 function registerTFGBakeliteRecipes(event) {
 
-	// TODO: move oil PE to MV
+	// Make PE exclusively MV+, it's fine if the LCR recipe stays LV since you can't make an LCR in LV anyway
+	event.recipes.gtceu.chemical_reactor('gtceu:polyethylene_from_oxygen')
+		.inputFluids('gtceu:oxygen 1000', 'gtceu:ethylene 144')
+		.circuit(1)
+		.outputFluids('gtceu:polyethylene 216')
+		.duration(2 * 20)
+		.EUt(GTValues.VA[GTValues.MV])
 
+	event.recipes.gtceu.chemical_reactor('gtceu:polyethylene_from_air')
+		.inputFluids('gtceu:air 1000', 'gtceu:ethylene 144')
+		.circuit(1)
+		.outputFluids('gtceu:polyethylene 144')
+		.duration(2 * 20)
+		.EUt(GTValues.VA[GTValues.MV])
+
+	// Formaldehyde
 	event.recipes.gtceu.chemical_reactor('tfg:formaldehyde_from_methane')
 		.inputFluids('gtceu:methane 1000', 'gtceu:oxygen 1000')
 		.outputFluids('gtceu:formaldehyde 1000', 'gtceu:hydrogen 2000')
@@ -53,18 +67,40 @@ function registerTFGBakeliteRecipes(event) {
 
 	event.remove({ id: 'gtceu:assembler/phenolic_board' })
 
-	event.recipes.gtceu.forming_press('tfg:phenolic_board')
+	event.recipes.gtceu.forming_press('tfg:phenolic_board_lv')
 		.itemInputs('2x #forge:foils/bakelite', '2x minecraft:paper', 'gtceu:resin_circuit_board')
 		.itemOutputs('gtceu:phenolic_circuit_board')
 		.duration(7.5 * 20)
 		.EUt(GTValues.VA[GTValues.LV])
 
-	event.recipes.gtceu.assembler('tfg:phenolic_board_pe')
+	event.recipes.gtceu.assembler('tfg:phenolic_board_mv')
+		.itemInputs('2x #forge:foils/bakelite', 'gtceu:wood_plate')
+		.inputFluids('gtceu:phenol 50')
+		.itemOutputs('gtceu:phenolic_circuit_board')
+		.duration(7.5 * 20)
+		.EUt(GTValues.VA[GTValues.MV])
+
+	event.recipes.gtceu.assembler('tfg:phenolic_board_mv_pe')
 		.itemInputs('2x #forge:foils/polyethylene', 'gtceu:wood_plate')
 		.inputFluids('gtceu:phenol 50')
 		.itemOutputs('gtceu:phenolic_circuit_board')
 		.duration(7.5 * 20)
 		.EUt(GTValues.VA[GTValues.MV])
 
+	// machine hull changes/additions
 	event.replaceInput({ id: 'gtceu:shaped/mv_machine_hull' }, '#forge:plates/wrought_iron', '#forge:plates/bakelite')
+
+	event.recipes.gtceu.assembler('tfg:ulv_hull_bakelite')
+		.itemInputs('gtceu:ulv_machine_casing', '2x #forge:single_cables/red_alloy')
+		.inputFluids('tfg:bakelite 432')
+		.itemOutputs('gtceu:ulv_machine_hull')
+		.duration(25)
+		.EUt(16)
+
+	event.recipes.gtceu.assembler('tfg:lv_hull_bakelite')
+		.itemInputs('gtceu:lv_machine_casing', '2x #forge:single_cables/tin')
+		.inputFluids('tfg:bakelite 432')
+		.itemOutputs('gtceu:lv_machine_hull')
+		.duration(50)
+		.EUt(16)
 }
