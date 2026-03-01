8ca01fd7d981beae8de1a76793070871c11bbd08
Sakura Kitsurugi <sakura.kitsurugi@proton.me>
2026-03-01 20:33:46 +0200

    Create: Copycats+ Integration

---
+++ kubejs/startup_scripts/create_copycats/constants.js
@@ -0,0 +1,16 @@
+// priority: -100
+"use strict";
+
+global.CREATE_COPYCATS_DISABLED_ITEMS = [
+  'copycats:copycat_iron_door',
+  'copycats:copycat_iron_trapdoor',
+  'copycats:copycat_wooden_button',
+  'copycats:copycat_stone_button',
+  'copycats:copycat_wooden_pressure_plate',
+  'copycats:copycat_stone_pressure_plate',
+  'copycats:copycat_light_weighted_pressure_plate',
+  'copycats:copycat_heavy_weighted_pressure_plate',
+  'copycats:copycat_shaft',
+  'copycats:copycat_cogwheel',
+  'copycats:copycat_large_cogwheel'
+];
