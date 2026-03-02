cd3742dd69bf403a9360c67f8e859ac22a2410be
TomPlop <tomdidome@gmail.com>
2026-03-02 00:37:11 -0500

    Add MoSiB Line

--- kubejs/startup_scripts/tfg/tag_prefixes.js
+++ kubejs/startup_scripts/tfg/tag_prefixes.js
@@ -23,6 +23,44 @@ function registerTFGTagPrefixes(event) {
 
 	TagPrefix.nugget.setIgnored(GTMaterials.get('tfg:magnesia_refractory_brick'))
 	TagPrefix.block.setIgnored(GTMaterials.get('tfg:magnesia_refractory_brick'))
+
+	TagPrefix.block.setIgnored(GTMaterials.get('tfg:weak_inconel_718'))
+	TagPrefix.block.setIgnored(GTMaterials.get('tfg:homogenized_inconel_718'))
+	TagPrefix.nugget.setIgnored(GTMaterials.get('tfg:weak_inconel_718'))
+	TagPrefix.nugget.setIgnored(GTMaterials.get('tfg:homogenized_inconel_718'))
+	TagPrefix.dustTiny.setIgnored(GTMaterials.get('tfg:weak_inconel_718'))
+	TagPrefix.dustTiny.setIgnored(GTMaterials.get('tfg:homogenized_inconel_718'))
+	TagPrefix.dustSmall.setIgnored(GTMaterials.get('tfg:weak_inconel_718'))
+	TagPrefix.dustSmall.setIgnored(GTMaterials.get('tfg:homogenized_inconel_718'))
+	TagPrefix.dust.setIgnored(GTMaterials.get('tfg:homogenized_inconel_718'))
+
+	TagPrefix.dustTiny.setIgnored(GTMaterials.get('tfg:dirty_mo_si_b'))
+	TagPrefix.dustSmall.setIgnored(GTMaterials.get('tfg:dirty_mo_si_b'))
+
+	TagPrefix.block.setIgnored(GTMaterials.get('tfg:weak_mo_si_b'))
+	TagPrefix.nugget.setIgnored(GTMaterials.get('tfg:weak_mo_si_b'))
+	TagPrefix.dustTiny.setIgnored(GTMaterials.get('tfg:weak_mo_si_b'))
+	TagPrefix.dustSmall.setIgnored(GTMaterials.get('tfg:weak_mo_si_b'))
+	TagPrefix.ingot.setIgnored(GTMaterials.get('tfg:weak_mo_si_b'))
+
+	TagPrefix.block.setIgnored(GTMaterials.get('tfg:worked_mo_si_b'))
+	TagPrefix.nugget.setIgnored(GTMaterials.get('tfg:worked_mo_si_b'))
+	TagPrefix.dustTiny.setIgnored(GTMaterials.get('tfg:worked_mo_si_b'))
+	TagPrefix.dustSmall.setIgnored(GTMaterials.get('tfg:worked_mo_si_b'))
+	TagPrefix.ingot.setIgnored(GTMaterials.get('tfg:worked_mo_si_b'))
+	TagPrefix.dust.setIgnored(GTMaterials.get('tfg:worked_mo_si_b'))
+
+	TagPrefix.block.setIgnored(GTMaterials.get('tfg:homogenized_mo_si_b'))
+	TagPrefix.nugget.setIgnored(GTMaterials.get('tfg:homogenized_mo_si_b'))
+	TagPrefix.dustTiny.setIgnored(GTMaterials.get('tfg:homogenized_mo_si_b'))
+	TagPrefix.dustSmall.setIgnored(GTMaterials.get('tfg:homogenized_mo_si_b'))
+	TagPrefix.dust.setIgnored(GTMaterials.get('tfg:homogenized_mo_si_b'))
+
+	TagPrefix.block.setIgnored(GTMaterials.get('tfg:activated_mo_si_b'))
+	TagPrefix.nugget.setIgnored(GTMaterials.get('tfg:activated_mo_si_b'))
+	TagPrefix.dustTiny.setIgnored(GTMaterials.get('tfg:activated_mo_si_b'))
+	TagPrefix.dustSmall.setIgnored(GTMaterials.get('tfg:activated_mo_si_b'))
+	TagPrefix.dust.setIgnored(GTMaterials.get('tfg:activated_mo_si_b'))
 	
 	TagPrefix.nugget.setIgnored(GTMaterials.get('tfg:rough_silicon_carbide'))
 	TagPrefix.block.setIgnored(GTMaterials.get('tfg:rough_silicon_carbide'))
