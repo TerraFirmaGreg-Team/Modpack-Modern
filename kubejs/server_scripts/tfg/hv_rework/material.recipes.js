0493dbdf28703fdcbc967c4c291e187a14055049
Pyritie <pyritie@gmail.com>
2026-03-08 19:35:26 +0000

    balances the sulfur in igneous felsic so it's not an infinite source of it

--- kubejs/server_scripts/tfg/hv_rework/material.recipes.js
+++ kubejs/server_scripts/tfg/hv_rework/material.recipes.js
@@ -103,9 +103,9 @@ function registerTFGHVMaterialRecipes(event) {
 
 	event.recipes.gtceu.large_chemical_reactor('tfg:aluminium_sulfate_lcr')
 		.itemInputs(Item.of('tfg:igneous_felsic_dust', 1))
-		.inputFluids(Fluid.of('gtceu:sulfuric_acid', 1000))
+		.inputFluids(Fluid.of('gtceu:sulfuric_acid', 2000))
 		.itemOutputs(Item.of('tfg:aluminium_sulfate_dust', 1), Item.of('gtceu:potassium_sulfate_dust', 1), Item.of('gtceu:silicon_dioxide_dust'))
-		.outputFluids(Fluid.of('minecraft:water', 1000), Fluid.of('gtceu:diluted_sulfuric_acid', 1000))
+		.outputFluids(Fluid.of('minecraft:water', 1000))
 		.duration(20 * 84)
 		.EUt(GTValues.VA[GTValues.LV])
 		.circuit(2)
