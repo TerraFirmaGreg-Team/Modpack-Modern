6b112ec28bc452a68600d789ffc5a1c63803ab41
Redeix <brayden.j.m.ford@gmail.com>
2026-03-10 02:40:29 -0500

    textures never stop

--- kubejs/server_scripts/starcatcher/data.js
+++ kubejs/server_scripts/starcatcher/data.js
@@ -20,8 +20,8 @@ function registerStarcatcherHeatData(event) {
 	const filetTypes = new Set(Object.values(global.STARCATCHER_FISH).map(f => f.filet).filter(Boolean));
 	filetTypes.forEach(filet => {
 		if (filet !== null && filet !== undefined && filet !== 'calamari') {
-			event.itemHeat(`tfg:food/raw_${filet}_cut_seafood`, 1.0, null, null)
-			event.itemHeat(`tfg:food/cooked_${filet}_cut_seafood`, 1.0, null, null)
+			event.itemHeat(`tfg:food/seafood/raw_${filet}_cut`, 1.0, null, null)
+			event.itemHeat(`tfg:food/seafood/cooked_${filet}_cut`, 1.0, null, null)
 		};
 	});
 
@@ -47,96 +47,96 @@ function registerStarcatcherItemSize(event) {
 function registerStarcatcherFoodData(event) {
 	if (!Platform.isLoaded("starcatcher")) return;
 
-			event.foodItem(`tfg:food/raw_common_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/raw_common_cut`, (food) => {
 				food.hunger(3);
 				food.protein(1);
 				food.decayModifier(3);
 			});
-			event.foodItem(`tfg:food/cooked_common_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/cooked_common_cut`, (food) => {
 				food.hunger(4);
 				food.saturation(1);
 				food.protein(2);
 				food.decayModifier(2.25);
 			});
 
-			event.foodItem(`tfg:food/raw_uncommon_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/raw_uncommon_cut`, (food) => {
 				food.hunger(3);
 				food.protein(1.3);
 				food.decayModifier(3);
 			});
-			event.foodItem(`tfg:food/cooked_uncommon_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/cooked_uncommon_cut`, (food) => {
 				food.hunger(4);
 				food.saturation(1.1);
 				food.protein(2.3);
 				food.decayModifier(2.25);
 			});
 
-			event.foodItem(`tfg:food/raw_rare_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/raw_rare_cut`, (food) => {
 				food.hunger(3);
 				food.protein(1.7);
 				food.decayModifier(2.7);
 			});
-			event.foodItem(`tfg:food/cooked_rare_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/cooked_rare_cut`, (food) => {
 				food.hunger(4);
 				food.saturation(1.2);
 				food.protein(2.8);
 				food.decayModifier(2.2);
 			});
 
-			event.foodItem(`tfg:food/raw_epic_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/raw_epic_cut`, (food) => {
 				food.hunger(3);
 				food.protein(2.1);
 				food.decayModifier(2.5);
 			});
-			event.foodItem(`tfg:food/cooked_epic_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/cooked_epic_cut`, (food) => {
 				food.hunger(4);
 				food.saturation(1.4);
 				food.protein(3.3);
 				food.decayModifier(2.0);
 			});
 
-			event.foodItem(`tfg:food/raw_legendary_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/raw_legendary_cut`, (food) => {
 				food.hunger(3);
 				food.protein(2.3);
 				food.decayModifier(2.1);
 			});
-			event.foodItem(`tfg:food/cooked_legendary_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/cooked_legendary_cut`, (food) => {
 				food.hunger(4);
 				food.saturation(1.6);
 				food.protein(4.0);
 				food.decayModifier(1.8);
 			});
 
-			event.foodItem(`tfg:food/raw_shrimp_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/raw_shrimp_cut`, (food) => {
 				food.hunger(3);
 				food.protein(1);
 				food.decayModifier(3);
 			});
-			event.foodItem(`tfg:food/cooked_shrimp_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/cooked_shrimp_cut`, (food) => {
 				food.hunger(4);
 				food.saturation(1);
 				food.protein(2);
 				food.decayModifier(2.25);
 			});
 
-			event.foodItem(`tfg:food/raw_eel_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/raw_eel_cut`, (food) => {
 				food.hunger(3);
 				food.protein(1.7);
 				food.decayModifier(2.7);
 			});
-			event.foodItem(`tfg:food/cooked_eel_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/cooked_eel_cut`, (food) => {
 				food.hunger(4);
 				food.saturation(1.2);
 				food.protein(2.8);
 				food.decayModifier(2.2);
 			});
 
-			event.foodItem(`tfg:food/raw_crab_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/raw_crab_cut`, (food) => {
 				food.hunger(3);
 				food.protein(1.7);
 				food.decayModifier(2.7);
 			});
-			event.foodItem(`tfg:food/cooked_crab_cut`, (food) => {
+			event.foodItem(`tfg:food/seafood/cooked_crab_cut`, (food) => {
 				food.hunger(4);
 				food.saturation(1.2);
 				food.protein(2.8);
--- kubejs/startup_scripts/starcatcher/items.js
+++ kubejs/startup_scripts/starcatcher/items.js
@@ -9,30 +9,35 @@ function registerStarcatcherItems(event) {
 		const filetTypes = new Set(Object.values(global.STARCATCHER_FISH).map(f => f.filet).filter(Boolean));
 		filetTypes.forEach(filet => {
 
-			event.create(`tfg:food/raw_${filet}_cut`)
-				.translationKey(`item.tfg.food.raw_${filet}_cut`)
-				.food(food => food.hunger(2).saturation(1))
-				.tag('tfc:foods')
-				.tag('tfc:foods/meats')
-				.tag('tfc:foods/cooked_meats')
-				.tag('firmalife:foods/cooked_fish')
-				.tag('minecraft:fishes')
-				.tag('starcatcher:baits');
+			if (filet !== null && filet !== "calamari") {
+				event.create(`tfg:food/seafood/raw_${filet}_cut`)
+					.translationKey(`item.tfg.food.seafood.raw_${filet}_cut`)
+					.food(food => food.hunger(2).saturation(1))
+					.tag('tfc:foods')
+					.tag('tfc:foods/meats')
+					.tag('tfc:foods/cooked_meats')
+					.tag('firmalife:foods/cooked_fish')
+					.tag('minecraft:fishes')
+					.tag('starcatcher:baits');
 
-			event.create(`tfg:food/cooked_${filet}_cut`)
-				.translationKey(`item.tfg.food.cooked_${filet}_cut`)
-				.food(food => food.hunger(4).saturation(2))
-				.tag('tfc:foods')
-				.tag('tfc:foods/meats')
-				.tag('tfc:foods/raw_meats')
-				.tag('firmalife:foods/raw_fish')
-				.tag('minecraft:fishes');
+				event.create(`tfg:food/seafood/cooked_${filet}_cut`)
+					.translationKey(`item.tfg.food.seafood.cooked_${filet}_cut`)
+					.food(food => food.hunger(4).saturation(2))
+					.tag('tfc:foods')
+					.tag('tfc:foods/meats')
+					.tag('tfc:foods/raw_meats')
+					.tag('firmalife:foods/raw_fish')
+					.tag('minecraft:fishes');
+			};
 
 		});
 
 		// Create cooked fish items for each fish type.
 		Object.keys(global.STARCATCHER_FISH).forEach(fish => {
-			if ( global.STARCATCHER_FISH[fish]?.dimension !== null && global.STARCATCHER_FISH[fish]?.dimension !== undefined ) {
+			
+			if ( global.STARCATCHER_FISH[fish]?.dimension !== null && 
+				global.STARCATCHER_FISH[fish]?.dimension !== undefined && 
+				global.STARCATCHER_FISH[fish]?.filet !== null ) {
 
 				event.create(`tfg:food/cooked_fish/cooked_${fish}`)
 					.translationKey(`item.tfg.food.cooked_fish.cooked_${fish}`)
