2b7d766df970a72a67207269e5cc6f03909e452f
Rosenlied <55555033+Rosenlied@users.noreply.github.com>
2026-03-21 15:26:05 -0700

    Add tags to brinza cheese?
    Signed-off-by: Rosenlied <55555033+Rosenlied@users.noreply.github.com>

---
+++ kubejs/server_scripts/tfc_gurman/tags.js
@@ -0,0 +1,10 @@
+// priority: 0
+"use strict";
+
+
+const registerTFCGurmanItemTags = (event) => {
+  // brinza tag fix
+  event.add('firmalife:foods/cheeses', 'tfc_gourmet:ox_brinza_slice')
+  event.add('firmalife:foods/cheeses', 'tfc_gourmet:sheep_brinza_slice')
+  event.add('firmalife:foods/cheeses', 'tfc_gourmet:alpaca_brinza_slice')
+}
