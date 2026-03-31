d5e1b2ec7f4b3043951afb0fa34c0ec03d8aa633
Froffy025 <scotttaco025@gmail.com>
2026-03-31 17:09:23 -0400

    i think that's how this is supposed to work?

---
+++ kubejs/server_scripts/tfc_gurman/tags.js
@@ -0,0 +1,9 @@
+// priority: 0
+"use strict";
+
+const registerTFCGurmanItemTags = (event) => {
+    event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:bulgogi')
+    event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:bratwurst')
+    event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:tandoori_chicken')  
+    event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:tonkatsu')
+}
