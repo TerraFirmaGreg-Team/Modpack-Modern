2d9497cacd66c223e5f39922d5e878fab8119498
Froffy025 <78222136+Froffy025@users.noreply.github.com>
2026-03-31 19:50:19 -0400

    amend dyslexia again
    Signed-off-by: Froffy025 <78222136+Froffy025@users.noreply.github.com>

--- kubejs/server_scripts/tfc_gurman/tags.js
+++ kubejs/server_scripts/tfc_gurman/tags.js
@@ -2,8 +2,8 @@
 "use strict";
 
 function registerTFCGurmanItemTags (event) {
-    event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:bulgogi')
-    event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:bratwurst')
-    event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:tandoori_chicken')  
-    event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:tonkatsu')
+    event.add('tfc:foods/usable_in_sandwich', 'tfc_gourmet:bulgogi')
+    event.add('tfc:foods/usable_in_sandwich', 'tfc_gourmet:bratwurst')
+    event.add('tfc:foods/usable_in_sandwich', 'tfc_gourmet:tandoori_chicken')  
+    event.add('tfc:foods/usable_in_sandwich', 'tfc_gourmet:tonkatsu')
 }
