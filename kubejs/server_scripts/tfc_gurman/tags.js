425fc37398ab270f94abc2a42f025e472b23d95f
Froffy025 <scotttaco025@gmail.com>
2026-03-31 17:16:39 -0400

    wait that's supposed to be a function i think

--- kubejs/server_scripts/tfc_gurman/tags.js
+++ kubejs/server_scripts/tfc_gurman/tags.js
@@ -1,7 +1,7 @@
 // priority: 0
 "use strict";
 
-const registerTFCGurmanItemTags = (event) => {
+function registerTFCGurmanItemTags (event) {
     event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:bulgogi')
     event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:bratwurst')
     event.add('tfg:foods/usable_in_sandwich', 'tfc_gourmet:tandoori_chicken')  
