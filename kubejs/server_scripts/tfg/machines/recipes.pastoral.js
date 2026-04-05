28f1590fe49f3a54b8751b20bae883330f3aa265
TomPlop <tomdidome@gmail.com>
2026-04-05 14:19:22 -0400

    fix recipes

--- kubejs/server_scripts/tfg/machines/recipes.pastoral.js
+++ kubejs/server_scripts/tfg/machines/recipes.pastoral.js
@@ -2,7 +2,7 @@
 
 function registerTFGPastoralRecipes(event) {
 
-    // Par entity type spécifique
+    // Entity type specific
     let milk_cow = event.recipes.gtceu.pastoral_engine('tfg:milk_cow')
         .outputFluids('minecraft:milk 1000')
 		.duration(20 * 5)
@@ -24,8 +24,8 @@ function registerTFGPastoralRecipes(event) {
         .circuit(3)
     TFGRecipeSchemaBindings.animalPresentEntity(milk_goat, 'tfc:goat')
 
-    // Par catégorie (inchangé)
-    let wool = event.recipes.gtceu.pastoral_engine('tfg:shear_sheep')
+    // Category
+    let wool = event.recipes.gtceu.pastoral_engine('tfg:shear_wool')
         .itemOutputs(Item.of('tfc:wool'))
 		.duration(20 * 5)
 		.EUt(GTValues.VA[GTValues.LV])
