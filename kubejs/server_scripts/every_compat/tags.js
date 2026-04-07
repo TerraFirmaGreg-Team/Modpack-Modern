228b028d2bb75caaf8f667800107d47dac9a5666
Pyritie <pyritie@gmail.com>
2026-04-07 23:06:49 +0100

    oops wrong tag

--- kubejs/server_scripts/every_compat/tags.js
+++ kubejs/server_scripts/every_compat/tags.js
@@ -9,11 +9,11 @@ const registerEveryCompatItemTags = (event) => {
 	event.add('c:hidden_from_recipe_viewers', 'everycomp:c/gtceu/rubber_window_pane')
 
 	global.TFC_WOOD_TYPES.forEach(wood => {
-		event.add('forge:glass_panes', `everycomp:c/tfc/${wood}_window`)
+		event.add('forge:glass', `everycomp:c/tfc/${wood}_window`)
 		event.add('forge:glass_panes', `everycomp:c/tfc/${wood}_window_pane`)
 	})
 	global.AFC_WOOD_TYPES.forEach(wood => {
-		event.add('forge:glass_panes', `everycomp:c/afc/${wood}_window`)
+		event.add('forge:glass', `everycomp:c/afc/${wood}_window`)
 		event.add('forge:glass_panes', `everycomp:c/afc/${wood}_window_pane`)
 	})
 	const AD_ASTRA = [
@@ -22,10 +22,14 @@ const registerEveryCompatItemTags = (event) => {
 		'strophar'
 	]
 	AD_ASTRA.forEach(wood => {
-		event.add('forge:glass_panes', `everycomp:c/ad_astra/${wood}_window`)
+		event.add('forge:glass', `everycomp:c/ad_astra/${wood}_window`)
 		event.add('forge:glass_panes', `everycomp:c/ad_astra/${wood}_window_pane`)
 	});
 
-	event.add('forge:glass_panes', `everycomp:c/wan_ancient_beasts/ginkgo_window`)
+	event.add('forge:glass', `everycomp:c/wan_ancient_beasts/ginkgo_window`)
 	event.add('forge:glass_panes', `everycomp:c/wan_ancient_beasts/ginkgo_window_pane`)
+	event.add('forge:glass', `everycomp:c/domum_ornamentum/cactus_window`)
+	event.add('forge:glass_panes', `everycomp:c/domum_ornamentum/cactus_window_pane`)
+	event.add('forge:glass', `everycomp:c/domum_ornamentum/cactus_extra_window`)
+	event.add('forge:glass_panes', `everycomp:c/domum_ornamentum/cactus_extra_window_pane`)
 }
