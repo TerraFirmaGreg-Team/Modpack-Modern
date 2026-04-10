dc7060d12a86fd8719207d2489f36156f3fcc98e
Froffy025 <78222136+Froffy025@users.noreply.github.com>
2026-04-10 15:10:35 -0400

    adds #bee_nature_restoration to some tfg flowers

--- kubejs/server_scripts/firmalife/tags.js
+++ kubejs/server_scripts/firmalife/tags.js
@@ -29,6 +29,21 @@ const registerFirmaLifeItemTags = (event) => {
     event.add('minecraft:leaves', 'firmalife:plant/fig_leaves')
 
     event.add('tfc:clay_recycle_5', '#firmalife:clay_recycle_5')
+
+    event.add('firmalife:rest')
+
+    event.add('firmalife:bee_restoration_plants', 'tfg:plant/azalea')    
+    event.add('firmalife:bee_restoration_plants', 'tfg:plant/buttercup')
+    event.add('firmalife:bee_restoration_plants', 'tfg:plant/cornflower')
+    event.add('firmalife:bee_restoration_plants', 'tfc:plant/rose')
+    event.add('firmalife:bee_restoration_plants', 'tfc:plant/hibiscus')
+    event.add('firmalife:bee_restoration_plants', 'tfg:plant/mountain_hullwort')
+    event.add('firmalife:bee_restoration_plants', 'tfg:plant/palash')
+    event.add('firmalife:bee_restoration_plants', 'tfg:plant/penwortel')
+    event.add('firmalife:bee_restoration_plants', 'tfg:plant/qantu')
+    event.add('firmalife:bee_restoration_plants', 'tfg:plant/ramirezella')
+    event.add('firmalife:bee_restoration_plants', 'tfg:plant/ramunda')
+    event.add('firmalife:bee_restoration_plants', 'tfg:plant/yellow_saxifrage')
 }
 
 const registerFirmaLifeBlockTags = (event) => {
