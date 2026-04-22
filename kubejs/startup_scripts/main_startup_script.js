1e00b13b91b89bf9bc5e839028702aa3db4fe4a7
Sakura <sakura.kitsurugi@proton.me>
2026-04-22 03:29:29 +0300

    leaves (#3861)

--- kubejs/startup_scripts/main_startup_script.js
+++ kubejs/startup_scripts/main_startup_script.js
@@ -12,6 +12,7 @@ StartupEvents.registry('block', event => {
 BlockEvents.modification(event => {
 	registerAdAstraBlockModifications(event)
 	registerSpeciesBlockModifications(event)
+	registerMinecraftBlockModifications(event)
 })
 
 ItemEvents.modification(event => {
