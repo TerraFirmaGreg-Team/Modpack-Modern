4ddf99e2f6a548f55ca549dc9c2b574cc0a7a43b
Pyritie <pyritie@gmail.com>
2026-04-25 12:10:11 +0100

    buffed dye production

--- kubejs/server_scripts/tfc/recipes.dyes.js
+++ kubejs/server_scripts/tfc/recipes.dyes.js
@@ -133,23 +133,23 @@ function registerTFCDyeRecipes(event) {
 				inputArray.push(`#forge:dyes/${dyeName}`);
 			}
 
-			event.recipes.tfc.pot(inputArray, Fluid.of('tfc:vinegar', 250 * i), 200, 200)
-				.fluidOutput(Fluid.of(`tfc:${dyeName}_dye`, 144 * i))
+			event.recipes.tfc.pot(inputArray, Fluid.of('tfc:vinegar', 144 * i), 200, 200)
+				.fluidOutput(Fluid.of(`tfc:${dyeName}_dye`, 288 * i))
 				.id(`tfc:pot/${i}x_${dyeName}_dye`)
 		}
 
 		event.recipes.gtceu.chemical_reactor(`chemical_dye_${dyeName}_acetic`)
 			.itemInputs(`#forge:dyes/${dyeName}`)
-			.inputFluids('gtceu:acetic_acid 250', "#tfg:clean_water 1000")
-			.outputFluids(Fluid.of(`tfc:${dyeName}_dye`, 144 * 3))
+			.inputFluids('gtceu:acetic_acid 25', "#tfg:clean_water 475")
+			.outputFluids(Fluid.of(`tfc:${dyeName}_dye`, 7 * 144))
 			.duration(15 * 20)
 			.EUt(24)
 
 		event.recipes.gtceu.chemical_reactor(`chemical_dye_${dyeName}_vinegar`)
 			.itemInputs(`#forge:dyes/${dyeName}`)
-			.inputFluids(Fluid.of('tfc:vinegar', 250))
-			.outputFluids(Fluid.of(`tfc:${dyeName}_dye`, 144))
-			.duration(30 * 20)
+			.inputFluids(Fluid.of('tfc:vinegar', 144))
+			.outputFluids(Fluid.of(`tfc:${dyeName}_dye`, 288))
+			.duration(15 * 20)
 			.EUt(24)
 	})
 
