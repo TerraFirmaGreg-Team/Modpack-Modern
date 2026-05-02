3dd9b076b9b4f5f8b714aead718dc2a3c71ed4a1
Pyritie <pyritie@gmail.com>
2026-05-02 12:38:07 +0100

    fix wrought iron bridges missing

---
+++ kubejs/startup_scripts/macaws_for_tfc/creative_tab.js
@@ -0,0 +1,8 @@
+// priority: 0
+"use strict";
+
+StartupEvents.modifyCreativeTab('mcw_tfc_aio:bridgesitmegroup', event => {
+    event.add('mcw_tfc_aio:bridges/iron_bridges/iron_bridge')
+    event.add('mcw_tfc_aio:bridges/iron_bridges/iron_bridge_pier')
+    event.add('mcw_tfc_aio:bridges/iron_bridges/iron_bridge_stair')
+})
