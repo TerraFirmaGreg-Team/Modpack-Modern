e81a569fdacaaf83c5725f07ac01381c35e75824
Sakura <sakura.kitsurugi@proton.me>
2026-05-03 19:33:32 +0300

    Crafting Station Blacklist (#3951)
    * blacklist
    
    * block some gtceu blocks

--- kubejs/server_scripts/crafting_station/tags.js
+++ kubejs/server_scripts/crafting_station/tags.js
@@ -7,3 +7,9 @@ const registerCraftingStationBlockTags = (event) => {
 	event.add('c:hidden_from_recipe_viewers', 'craftingstation:crafting_station')
     event.add('c:hidden_from_recipe_viewers', 'craftingstation:crafting_station_slab')
 }
+
+const registerCraftingStationBlockEntityTags = (event) => {
+    global.CF_DISABLED_STORAGES.forEach(storage => {
+        event.add('craftingstation:blacklisted', storage)
+    })
+}
