06e6dfe280d8fc22e28bb245a0fce4747107608f
Sakura <sakura.kitsurugi@proton.me>
2026-05-03 22:21:26 +0300

    whitelist

--- kubejs/server_scripts/crafting_station/tags.js
+++ kubejs/server_scripts/crafting_station/tags.js
@@ -9,7 +9,7 @@ const registerCraftingStationBlockTags = (event) => {
 }
 
 const registerCraftingStationBlockEntityTags = (event) => {
-    global.CF_DISABLED_STORAGES.forEach(storage => {
-        event.add('craftingstation:blacklisted', storage)
+    global.CF_WHITELISTED_STORAGES.forEach(storage => {
+        event.add('craftingstation:whitelisted', storage)
     })
 }
