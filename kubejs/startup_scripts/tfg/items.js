2492d85725e80ff2730d17235a58bd27e929f683
闫茂源 <yanmaoyuan@baijiayun.com>
2026-05-11 21:09:52 +0800

    feat: register stencil items

--- kubejs/startup_scripts/tfg/items.js
+++ kubejs/startup_scripts/tfg/items.js
@@ -1,6 +1,8 @@
 // priority: 0
 "use strict";
 
+const TFGAsphaltRoadMarkings = Java.loadClass('su.terrafirmagreg.core.common.block.asphalt.TFGAsphaltRoadMarkings')
+
 /**
  * 
  * @param {Registry.Item} event 
@@ -24,6 +26,16 @@ const registerTFGItems = (event) => {
 	event.create('tfg:tar_chunk')
 	event.create('tfg:asphalt_binder')
 	event.create('tfg:asphalt_rubble')
+	event.create('tfg:asphalt_road_stencil_line')
+		.tag('tfg:road_marking_stencils')
+	event.create('tfg:asphalt_road_stencil_cross')
+		.tag('tfg:road_marking_stencils')
+	event.create('tfg:asphalt_road_stencil_arrow')
+		.tag('tfg:road_marking_stencils')
+
+	TFGAsphaltRoadMarkings.registerStencil('tfg:asphalt_road_stencil_line', 'line')
+	TFGAsphaltRoadMarkings.registerStencil('tfg:asphalt_road_stencil_cross', 'cross')
+	TFGAsphaltRoadMarkings.registerStencil('tfg:asphalt_road_stencil_arrow', 'arrow')
 
 	event.create('tfg:terra_firma_greg')
 		.tag('c:hidden_from_recipe_viewers')
