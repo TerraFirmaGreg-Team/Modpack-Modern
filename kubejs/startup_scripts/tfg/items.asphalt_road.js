e2275d9984a52ba1675290c5a195749f867f3f66
闫茂源 <yanmaoyuan@baijiayun.com>
2026-05-12 17:46:11 +0800

    feat: mv stencil items into core

--- kubejs/startup_scripts/tfg/items.asphalt_road.js
+++ kubejs/startup_scripts/tfg/items.asphalt_road.js
@@ -1,23 +1,9 @@
 // priority: 0
 "use strict";
-const TFGAsphaltRoadMarkings = Java.loadClass('su.terrafirmagreg.core.common.block.asphalt.TFGAsphaltRoadMarkings')
 
 function registerTFGAsphaltRoadItems(event) {
 
 	event.create('tfg:tar_chunk')
 	event.create('tfg:asphalt_binder')
 	event.create('tfg:asphalt_rubble')
-
-    let masks = [
-        "arrow", "line", "line_slash_l", "line_slash_r", 
-        "slash_l", "slash_r", "cross", "t", "corner",
-        "num_0", "num_1", "num_2", "num_3", "num_4",
-        "num_5", "num_6", "num_7", "num_8", "num_9",
-        "number"
-    ];
-    masks.forEach(mask => {
-        let itemId = `tfg:asphalt_road_stencil_${mask}`;
-        event.create(itemId).tag('tfg:road_marking_stencils');
-        TFGAsphaltRoadMarkings.registerStencil(itemId, mask);
-    });
 }
