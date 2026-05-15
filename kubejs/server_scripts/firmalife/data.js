05036b00d54febadca0891c2958d457b0e0ada00
ashlen <thesadru@gmail.com>
2026-05-15 20:20:07 +0200

    Fix firmalife dupe with crafting table (#4038)

--- kubejs/server_scripts/firmalife/data.js
+++ kubejs/server_scripts/firmalife/data.js
@@ -11,6 +11,14 @@ const registerFirmalifeItemSize = (event) => {
     event.itemSize('firmalife:jar/compost', 'tiny', 'medium', 'jar_of_compost')
     event.itemSize('firmalife:jar/rotten_compost', 'tiny', 'medium', 'jar_of_rotten_compost')
     event.itemSize('firmalife:jar/guano', 'tiny', 'medium', 'jar_of_guano')
-
+    
+    // fix firmalife food dupe patch
+	event.itemSize('firmalife:food/cooked_pizza', 'very_small', 'medium')
+	event.itemSize('firmalife:food/burrito', 'very_small', 'medium')
+	event.itemSize('firmalife:food/taco', 'very_small', 'medium')
+	event.itemSize('firmalife:food/cooked_pie', 'very_small', 'medium')
+	event.itemSize('firmalife:food/filled_pie', 'very_small', 'medium')
+	event.itemSize('firmalife:food/raw_pizza', 'very_small', 'medium')
+	event.itemSize('firmalife:food/stinky_soup', 'very_small', 'medium')
 }
 //#endregion
