7b66ca8855222a4a98694b1ff1e30d1e9cefd8b5
Sakura <sakura.kitsurugi@proton.me>
2026-06-03 01:30:01 +0300

    extra spawns due to odd variant issues

--- kubejs/server_scripts/tfg/slimes/slimes.tags.js
+++ kubejs/server_scripts/tfg/slimes/slimes.tags.js
@@ -7,9 +7,17 @@ function registerTFGSlimeItemTags(event) {
 
 function registerTFGSlimeBiomeTags(event) {
     event.add('tfg:nether/plant_slime_habitat', 'tfg:nether/muggy_bog')
+    event.add('tfg:nether/plant_slime_habitat', 'tfg:nether/thorny_brambles')
+
     event.add('tfg:nether/glowberry_slime_habitat', 'tfg:nether/lush_hollow')
+
     event.add('tfg:nether/spring_slime_habitat', 'tfg:nether/geothermal_springs')
+
     event.add('tfg:nether/ice_slime_habitat', 'tfg:nether/salt_caves')
+
     event.add('tfg:nether/lava_slime_habitat', 'tfg:nether/lava_floes')
+    event.add('tfg:nether/lava_slime_habitat', 'tfg:nether/ash_forest')
+
     event.add('tfg:nether/latex_slime_habitat', 'tfg:nether/webbed_lair')
+
 }
