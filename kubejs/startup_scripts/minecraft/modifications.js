75ab951b6612525b2cfdeb4c6039b68dfb81d5a0
mafien0 <mafien_0@tuta.io>
2026-06-11 22:48:39 +0500

    Remove wool and wool carpets as a fuel source

--- kubejs/startup_scripts/minecraft/modifications.js
+++ kubejs/startup_scripts/minecraft/modifications.js
@@ -19,11 +19,20 @@ function registerMinecraftItemModifications(event) {
 		item.maxDamage = 960
 	})
 
-	// Removes lava buckets as fuel
+	// Remove unneeded fuel sources
 
-	event.modify('minecraft:lava_bucket', item => {
-		item.burnTime = 0
-	})
+	event.modify("minecraft:lava_bucket", (item) => {
+		item.burnTime = 0;
+	});
+
+	global.MINECRAFT_DYE_NAMES.forEach((color) => {
+		event.modify(`minecraft:${color}_wool`, (item) => {
+			item.burnTime = 0;
+		});
+		event.modify(`minecraft:${color}_carpet`, (item) => {
+			item.burnTime = 0;
+		});
+	});
 
 	// Ladder burn time
 
