1bd0cd55436410bd161f19a532bc42ef6a3e25c1
mafien0 <mafien_0@tuta.io>
2026-06-11 22:05:33 +0500

    Remove wool and wool carpets as a fuel source

--- kubejs/startup_scripts/minecraft/modifications.js
+++ kubejs/startup_scripts/minecraft/modifications.js
@@ -2,40 +2,48 @@
 "use strict";
 
 function registerMinecraftItemModifications(event) {
-	event.modify('minecraft:popped_chorus_fruit', item => {
-		item.foodProperties = food => {
-			food.hunger(2)
-			food.saturation(1)
-		}
-	})
+    event.modify("minecraft:popped_chorus_fruit", (item) => {
+        item.foodProperties = (food) => {
+            food.hunger(2);
+            food.saturation(1);
+        };
+    });
 
-	event.modify('minecraft:elytra', item => {
-		item.maxDamage = 2046
-	})
+    event.modify("minecraft:elytra", (item) => {
+        item.maxDamage = 2046;
+    });
 
-	// Matches blue steel greaves
+    // Matches blue steel greaves
 
-	event.modify('minecraft:netherite_leggings', item => {
-		item.maxDamage = 960
-	})
+    event.modify("minecraft:netherite_leggings", (item) => {
+        item.maxDamage = 960;
+    });
 
-	// Removes lava buckets as fuel
+    // Remove unneeded fuel sources
 
-	event.modify('minecraft:lava_bucket', item => {
-		item.burnTime = 0
-	})
+    event.modify("minecraft:lava_bucket", (item) => {
+        item.burnTime = 0;
+    });
 
-	// Ladder burn time
+    global.MINECRAFT_DYE_NAMES.forEach((color) => {
+        event.modify(`minecraft:${color}_wool`, (item) => {
+            item.burnTime = 0;
+        });
+        event.modify(`minecraft:${color}_carpet`, (item) => {
+            item.burnTime = 0;
+        });
+    });
 
-	event.modify('minecraft:ladder', item => {
-		item.burnTime = 100
-	})
+    // Ladder burn time
+
+    event.modify("minecraft:ladder", (item) => {
+        item.burnTime = 100;
+    });
 }
 
 function registerMinecraftBlockModifications(event) {
-
-	event.modify('minecraft:cherry_leaves', block => {
-		block.hasCollision = false
-		block.speedFactor = 0.25
-	})
+    event.modify("minecraft:cherry_leaves", (block) => {
+        block.hasCollision = false;
+        block.speedFactor = 0.25;
+    });
 }
