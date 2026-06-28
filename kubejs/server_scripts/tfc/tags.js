b4aebc5e161c7a8c0dacfef86dfc5012f9ecf461
Pyritie <pyritie@gmail.com>
2026-06-28 14:41:03 +0100

    fix lighters

--- kubejs/server_scripts/tfc/tags.js
+++ kubejs/server_scripts/tfc/tags.js
@@ -490,8 +490,6 @@ function registerTFCItemTags(event) {
 
     event.add('tfc:starts_fires_with_items', 'gtceu:matches');
     event.add('tfc:starts_fires_with_durability', 'gtceu:matchbox');
-    event.add('tfc:starts_fires_with_durability', 'gtceu:invar_lighter');
-    event.add('tfc:starts_fires_with_durability', 'gtceu:platinum_lighter');
     event.add('forge:tools/igniter', 'tfc:firestarter');
 
     //TODO: properly allow GT fluid containers to work with TFC crafting recipes.
