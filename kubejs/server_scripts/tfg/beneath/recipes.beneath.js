eabb96e947f5aa6f1098491ae7fb385302e1377c
OLOXpl <oliwierwoj01@gmail.com>
2026-07-09 19:51:01 +0200

    mossing^2
    yes

--- kubejs/server_scripts/tfg/beneath/recipes.beneath.js
+++ kubejs/server_scripts/tfg/beneath/recipes.beneath.js
@@ -6,13 +6,28 @@
  * @param {Internal.RecipesEventJS_} event 
  */
 function registerTFGBeneathRecipes(event) {
-	
-	// Moss
+	const nonCarpet_MossCarpets = [
+		'tfc:plant/moss','tfc:plant/spanish_moss',
+		'tfc:plant/reindeer_lichen','tfc:plant/cobblestone_lichen'
+	];
 
-	event.shapeless('1x minecraft:moss_block', [
-		'#tfc:dirt',
-		'#tfc:moss'
-	]).id(`tfg:shapeless/moss_block`)
+	for (let i = 0; i < nonCarpet_MossCarpets.length; i++) {
+		event.shapeless('1x minecraft:moss_block', [
+			'#tfc:dirt',
+			nonCarpet_MossCarpets[i]
+		]).id(`tfg:shapeless/moss_block/${nonCarpet_MossCarpets[i]}`)
+	}
+
+	const realMossCarpets = [
+		'minecraft:moss_','tfg:pale_moss_','wan_ancient_beasts:olden_moss_'
+	];
+
+	for (let i = 0; i < realMossCarpets.length; i++) {
+		event.shapeless(`${realMossCarpets[i]}block`, [
+			'#tfc:dirt',
+			realMossCarpets[i] + "carpet"
+		]).id(`tfg:shapeless/${realMossCarpets[i]}block`)
+	}
 
 	event.shaped('6x minecraft:moss_carpet', [
 		'AA'
@@ -31,4 +46,5 @@ function registerTFGBeneathRecipes(event) {
 	], {
 		A: 'wan_ancient_beasts:olden_moss_block'
 	}).id(`tfg:shaped/olden_moss_carpet`)
+	
 }
--- kubejs/server_scripts/wan_ancient_beasts/recipes.js
+++ kubejs/server_scripts/wan_ancient_beasts/recipes.js
@@ -135,16 +135,4 @@ const registerWABRecipes = (event) => {
 			0, 0, 1, 0, 0, 0, 1, 0
 		]
 	)
-
-	event.shapeless('wan_ancient_beasts:olden_moss_block', [
-		'wan_ancient_beasts:olden_moss_carpet',
-		'wan_ancient_beasts:olden_moss_carpet',
-		'wan_ancient_beasts:olden_moss_carpet'
-	]).id('wan_ancient_beasts:olden_moss_block')
-
-	event.shapeless('tfg:pale_moss_block',[
-		'tfg:pale_moss_carpet',
-		'tfg:pale_moss_carpet',
-		'tfg:pale_moss_carpet'
-	]).id('tfg:pale_moss_block')
 }
