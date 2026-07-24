6faf1460c0f4ac4bf156826aa983d2a56c9db990
hollow-earth <vanctardi@protonmail.com>
2026-07-23 23:15:26 -0400

    Feature: added electrolyzer recipes for quartz and smoky quartz dusts

--- kubejs/server_scripts/tfg/ores_and_materials/recipes.quartzes.js
+++ kubejs/server_scripts/tfg/ores_and_materials/recipes.quartzes.js
@@ -22,7 +22,21 @@ function registerTFGQuartzRecipes(event) {
 		.itemOutputs('1x #forge:dusts/nether_quartz', '8x minecraft:redstone')
 		.duration(7 * 20)
 		.EUt(60)
-	//#endregion
+
+	event.recipes.gtceu.electrolyzer('electrolyze_smoky_quartz_dust')
+		.itemInputs('3x #forge:dusts/nether_quartz')
+		.itemOutputs('gtceu:silicon_dust')
+		.outputFluids(Fluid.of('gtceu:oxygen', 2000))
+		.duration(7 * 20)
+		.EUt(60)
+
+	event.recipes.gtceu.electrolyzer('electrolyze_quartz_dust')
+		.itemInputs('3x #forge:dusts/quartzite')
+		.itemOutputs('gtceu:silicon_dust')
+		.outputFluids(Fluid.of('gtceu:oxygen', 2000))
+		.duration(7 * 20)
+		.EUt(60)
+  //#endregion
 
 	//#region Glass
 
