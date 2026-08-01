d57d2bee562dd9156be43563e42ad7ca0410457c
Sakura <sakura.kitsurugi@proton.me>
2026-08-02 01:29:37 +0300

    tags

--- kubejs/server_scripts/jellies/tags.js
+++ kubejs/server_scripts/jellies/tags.js
@@ -1,8 +1,38 @@
 // priority: 0
 "use strict";
 
+// I will add a general tag to all jellies in next update
+const allJellies = [
+	'biotite',
+	'certus',
+	'glowberry',
+	'herbal',
+	'ice',
+	'latex',
+	'lava',
+	'pentetic',
+	'phosphorum',
+	'plant',
+	'rock',
+	'spring',
+	'pyritie',
+	'eevee'
+]
+
 function registerJelliesItemTags(event) {
-	event.add("jellies:jellie_food", "#beneath:mushrooms");
+	event.add("jellies:jellie_food", "#forge:mushrooms");
 	event.add("jellies:jellie/slime_ball/plant", "#tfc:compost_greens")
 	event.add("jellies:jellie/slime_ball/phosphorum", "#tfc:compost_browns")
+}
+
+function registerJelliesEntityTags(event) {
+	event.add('tfc:vanilla_monsters', 'jellies:rock')
+
+	allJellies.forEach(jellie => {
+		event.add('ad_astra:lives_without_oxygen', `jellies:${jellie}`)
+		event.add('ad_astra:can_survive_extreme_cold', `jellies:${jellie}`)
+		event.add('ad_astra:can_survive_extreme_heat', `jellies:${jellie}`)
+		event.add('ad_astra:can_survive_in_space', `jellies:${jellie}`)
+		event.add('ad_astra:can_survive_in_acid_rain', `jellies:${jellie}`)
+	})
 }
