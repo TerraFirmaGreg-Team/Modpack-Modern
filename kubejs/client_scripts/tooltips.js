7d0952f8d51cae6d5a9ca8360e72fdd637cfcb5f
ashlen <thesadru@gmail.com>
2026-08-04 22:53:39 +0200

    Add °C to clothes

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -248,23 +248,19 @@ const registerTooltips = (event) => {
 	// Insulation
 	event.addAdvanced(['create:copper_diving_helmet', 'create:copper_backtank', 'create:copper_diving_boots'], (item, advanced, text) => {
 		text.add(1, Text.translate('tfg.tooltip.armor.copper_diving_suit_warmth'));
-		text.add(2, Text.translate('tfg.tooltip.armor.copper_diving_suit_insulation'));
+		text.add(2, Text.translate('tfg.tooltip.armor.copper_diving_suit_safety'));
 	})
 	event.addAdvanced(['create:netherite_diving_helmet', 'create:netherite_backtank', 'create:netherite_diving_boots', 'minecraft:netherite_leggings', 'minecraft:netherite_boots'], (item, advanced, text) => {
 		text.add(1, Text.translate('tfg.tooltip.armor.netherite_diving_suit_warmth'));
-		text.add(2, Text.translate('tfg.tooltip.armor.netherite_diving_suit_insulation'));
+		text.add(2, Text.translate('tfg.tooltip.armor.netherite_diving_suit_safety'));
 	})
 	event.addAdvanced(['gtceu:nanomuscle_helmet', 'gtceu:nanomuscle_chestplate', 'gtceu:nanomuscle_leggings', 'gtceu:nanomuscle_boots', 'gtceu:advanced_nanomuscle_chestplate'], (item, advanced, text) => {
-		text.add(1, Text.translate('tfg.tooltip.armor.nanomuscle_warmth'));
-		text.add(2, Text.translate('tfg.tooltip.armor.nanomuscle_insulation'));
 		text.add(3, Text.translate('tfg.tooltip.armor.nanomuscle_set'));
 		if (item === 'gtceu:nanomuscle_leggings') {
 			text.add(4, Text.translate('tfg.tooltip.armor.nanomuscle_legs_buff'));
 		}
 	})
 	event.addAdvanced(['gtceu:quarktech_helmet', 'gtceu:quarktech_chestplate', 'gtceu:quarktech_leggings', 'gtceu:quarktech_boots', 'gtceu:advanced_quarktech_chestplate'], (item, advanced, text) => {
-		text.add(1, Text.translate('tfg.tooltip.armor.quarktech_warmth'));
-		text.add(2, Text.translate('tfg.tooltip.armor.quarktech_insulation'));
 		text.add(3, Text.translate('tfg.tooltip.armor.quarktech_set'));
 	})
 	event.addAdvanced([
@@ -272,8 +268,6 @@ const registerTooltips = (event) => {
 		'ad_astra:netherite_space_helmet', 'ad_astra:netherite_space_suit', 'ad_astra:netherite_space_pants', 'ad_astra:netherite_space_boots',
 		'ad_astra:jet_suit_helmet', 'ad_astra:jet_suit', 'ad_astra:jet_suit_pants', 'ad_astra:jet_suit_boots'
 	], (item, advanced, text) => {
-		text.add(1, Text.translate('tfg.tooltip.armor.space_suit_warmth'));
-		text.add(2, Text.translate('tfg.tooltip.armor.space_suit_insulation'));
 		text.add(3, Text.translate('tfg.tooltip.armor.space_suit_set'));
 	})
 	event.addAdvanced(['sns:blue_steel_toe_hiking_boots'], (item, advanced, text) => {
