18c71fcf136941157e83a8d1d9e66b4433c6d48f
TomPlop <tomdidome@gmail.com>
2026-08-25 18:43:42 -0400

    recipes for Budding Charger

--- kubejs/server_scripts/tfg/machines/recipes.budding.js
+++ kubejs/server_scripts/tfg/machines/recipes.budding.js
@@ -1,28 +1,28 @@
 function registerTFGBuddingRecipes(event) {
 
     event.recipes.gtceu.budding_charger('tfg:tier_1')
-        .itemInputs('minecraft:copper_ingot')
+        .inputFluids(Fluid.of('minecraft:water', 4000))
         .addData("budding_charge", 25)
         .addData("budding_max_tier", 1)
         .EUt(GTValues.VA[GTValues.HV])
         .duration(20*10)
 
     event.recipes.gtceu.budding_charger('tfg:tier_2')
-        .itemInputs(Item.of('gold_ingot'))
+        .itemInputs(Item.of('ae2:certus_quartz_dust', 4))
         .addData("budding_charge", 25)
         .addData("budding_max_tier", 2)
         .EUt(GTValues.VA[GTValues.HV])
         .duration(20*10)
 
     event.recipes.gtceu.budding_charger('tfg:tier_3')
-        .itemInputs(Item.of('gtceu:tin_ingot'))
+        .itemInputs(Item.of('ae2:charged_certus_quartz_crystal'))
         .addData("budding_charge", 25)
         .addData("budding_max_tier", 3)
         .EUt(GTValues.VA[GTValues.HV])
         .duration(20*10)
 
     event.recipes.gtceu.budding_charger('tfg:tier_4')
-        .itemInputs(Item.of('gtceu:tungsten_ingot'))
+        .itemInputs(Item.of('ae2:fluix_crystal'))
         .addData("budding_charge", 25)
         .addData("budding_max_tier", 4)
         .EUt(GTValues.VA[GTValues.HV])
