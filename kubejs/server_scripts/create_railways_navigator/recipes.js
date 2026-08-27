8cc87f941cf31e37fe0b06c78c5af238bfed2ee8
felix <fel3web@gmail.com>
2026-08-27 12:52:45 +0100

    Add recipes

---
+++ kubejs/server_scripts/create_railways_navigator/recipes.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/**
+ * 
+ * @param {Internal.RecipesEventJS} event 
+ */
+const registerChimesRecipes = (event) => {
+	if (Platform.isLoaded("createrailwaysnavigator")) {
+        //Display variants and train station clock items already have default
+        //recipes. No need to change them.
+
+        //Display Board
+        event.shaped(
+            Item.Of('createrailwaysnavigator:advanced_display'),
+            [
+                "AAA",
+                "CBE",
+                "EDE"
+            ], {
+                A: 'gtceu:glass_plate',
+                B: 'create:display_board',
+                C: 'gtceu:computer_monitor_cover',
+                D: 'vintageimprovements:redstone_module',
+                E: 'gtceu:brass_screw'
+            }
+        )
+
+        //Navigator
+        event.shaped(
+            Item.Of('createrailwaysnavigator:navigator'),
+            [
+                "ABA",
+                "CDC",
+                "FEG"
+            ], {
+                A: 'gtceu:mv_sensor',
+                B: 'gtceu:glass_plate',
+                C: '#gtceu:circuits/mv',
+                D: 'gtceu:computer_monitor_cover',
+                E: 'vintageimprovements:redstone_module',
+                F: '#gtceu:batteries/mv',
+                G: 'gtceu:mv_emitter'
+            }
+        )
+	}
+}
