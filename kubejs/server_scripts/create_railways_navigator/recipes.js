183ae13efa71e3d4d1414fbecf6e7036a697b29d
felix <fel3web@gmail.com>
2026-08-27 21:28:29 +0100

    change recipes

--- kubejs/server_scripts/create_railways_navigator/recipes.js
+++ kubejs/server_scripts/create_railways_navigator/recipes.js
@@ -15,22 +15,23 @@ const registerRailwaysNavigatorRecipes = (event) => {
             [
                 "AAA",
                 "CBE",
-                "EDE"
+                "FDE"
             ], {
                 A: 'gtceu:glass_plate',
                 B: 'create:display_board',
                 C: 'gtceu:computer_monitor_cover',
-                D: 'vintageimprovements:redstone_module',
-                E: 'gtceu:brass_screw'
+                D: 'gtceu:small_red_alloy_gear',
+                E: 'gtceu:brass_screw',
+                F: '#forge:tools/screwdrivers'
             }
         )
         event.recipes.gtceu.assembler('assembler:advanced_display')
             .itemInputs(
                 'create:display_board',
                 'gtceu:computer_monitor_cover',
-                'vintageimprovements:redstone_module',
+                'gtceu:small_red_alloy_gear',
                 '3x gtceu:glass_plate',
-                '3x gtceu:brass_screw'
+                '2x gtceu:brass_screw'
             )
             .itemOutputs('2x createrailwaysnavigator:advanced_display')
             .duration(100)
@@ -41,28 +42,31 @@ const registerRailwaysNavigatorRecipes = (event) => {
         event.shaped(
             Item.of('createrailwaysnavigator:navigator'),
             [
-                "ABA",
-                "CDC",
+                "ABH",
+                "CDI",
                 "FEG"
             ], {
                 A: 'gtceu:mv_sensor',
                 B: 'gtceu:glass_plate',
                 C: '#gtceu:circuits/mv',
                 D: 'gtceu:computer_monitor_cover',
-                E: 'vintageimprovements:redstone_module',
+                E: 'gtceu:small_red_alloy_gear',
                 F: '#gtceu:batteries/mv',
-                G: 'gtceu:mv_emitter'
+                G: 'gtceu:mv_emitter',
+                H: '#forge:tools/screwdrivers',
+                I: 'gtceu:stainless_steel_screw'
             }
         )
         event.recipes.gtceu.assembler('assembler:navigator')
             .itemInputs(
                 'gtceu:computer_monitor_cover',
-                'vintageimprovements:redstone_module',
+                'gtceu:small_red_alloy_gear',
                 '#gtceu:batteries/mv',
                 'gtceu:mv_emitter',
                 'gtceu:glass_plate',
-                '2x #gtceu:circuits/mv',
-                '2x gtceu:mv_sensor'
+                '#gtceu:circuits/mv',
+                'gtceu:mv_sensor',
+                'gtceu:stainless_steel_screw'
             )
             .itemOutputs('createrailwaysnavigator:navigator')
             .duration(200)
