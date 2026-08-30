8a4a2a69fe88a437a0ac808871cc879c04d95f04
ashlen <thesadru@gmail.com>
2026-08-31 00:36:39 +0200

    add all the oxygen distributors to tags.environment.js

--- kubejs/server_scripts/tfg/environment/tags.environment.js
+++ kubejs/server_scripts/tfg/environment/tags.environment.js
@@ -63,7 +63,10 @@ const registerTFGEnvironmentBlockTags = (event) => {
         'ad_astra:vent',
         'ad_astra:oxygen_distributor',
         'ad_astra:gravity_normalizer',
-        'gtceu:oxygen_distributor',
+        'gtceu:oxygen_distributor_10',
+        'gtceu:oxygen_distributor_50',
+        'gtceu:oxygen_distributor_200',
+        'gtceu:oxygen_distributor_1000',
 
         // Frames, bars, fences
         'minecraft:iron_bars',
