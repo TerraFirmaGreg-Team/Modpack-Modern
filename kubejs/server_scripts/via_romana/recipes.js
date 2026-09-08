803b76e31c213d0c54a64cdd05d553f04cbc73ef
Robak132 <kubarob13@gmail.com>
2026-09-08 13:13:29 +0200

    Updated items to tags

--- kubejs/server_scripts/via_romana/recipes.js
+++ kubejs/server_scripts/via_romana/recipes.js
@@ -2,14 +2,14 @@
 "use strict";
 
 const registerViaRomanaRecipes = (event) => {
-	if (!Platform.isLoaded('via_romana')) return
+  if (!Platform.isLoaded('via_romana')) return
 
-	event.remove({ mod: 'via_romana' })
+  event.remove({ mod: 'via_romana' })
 
-	event.shapeless('via_romana:charting_map', [
-		'minecraft:map',
-		'#forge:dyes/black',
-		'minecraft:feather',
-		'minecraft:string'
-	]).id('via_romana:charting_map')
+  event.shapeless('via_romana:charting_map', [
+    'minecraft:map',
+    '#forge:dyes/black',
+    '#tfg:feathers',
+    '#forge:string'
+  ]).id('via_romana:charting_map')
 }
